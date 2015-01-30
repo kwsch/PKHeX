b01dc7dbfc8db90df99b80461a433d978f9c5875
Kurt <kaphotics@gmail.com>
2015-01-29 17:32:07 -0800

    Fix Splash Screen Image from Tiling

--- Misc/SplashScreen.Designer.cs
+++ Misc/SplashScreen.Designer.cs
@@ -56,6 +56,7 @@
             // pictureBox1
             // 
             this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.pictureBox1.Location = new System.Drawing.Point(2, 3);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(64, 64);
