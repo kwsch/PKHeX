042671f1930a9a8756ab7c24db414315daa1a48c
Kurt <kaphotics@gmail.com>
2015-02-24 20:15:49 -0800

    Add appropriate attribution for QR Code Image Gen

--- Misc/QR.cs
+++ Misc/QR.cs
@@ -20,6 +20,11 @@ namespace PKHeX
             PB_QR.BackgroundImage = qrCode.GetGraphic(10);
         }
     }
+
+    /// QR Code / Image Generation code via codebude/QRCoder on GitHub.
+    /// https://github.com/codebude/QRCoder
+    /// The QRCoder project is authored by Raffael Herrmann, licensed under the MIT License.
+    /// License: https://github.com/codebude/QRCoder/blob/master/LICENSE.txt
     public class QRCodeGenerator
     {
         private char[] alphanumEncTable = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', ' ', '$', '%', '*', '+', '-', '.', '/', ':' };
