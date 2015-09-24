cced16dff4d98d41b7db8053e0e01333294303f0
Kurt <kaphotics@gmail.com>
2015-09-24 00:01:15 -0700

    Tidy up splash screen code
    Clearer to just have a spinning thread instead of a timer.

--- Misc/SplashScreen.cs
+++ Misc/SplashScreen.cs
@@ -1,4 +1,4 @@
-﻿using System.Timers;
+﻿using System.Threading;
 using System.Windows.Forms;
 
 namespace PKHeX
@@ -8,20 +8,16 @@ namespace PKHeX
         public SplashScreen()
         {
             InitializeComponent();
-            System.Timers.Timer myTimer = new System.Timers.Timer();
-            myTimer.Elapsed += DisplayTimeEvent;
-            myTimer.Interval = 50; // milliseconds per trigger interval
-            myTimer.Start();
-        }
-        public void DisplayTimeEvent(object source, ElapsedEventArgs e)
-        {
-            if (!Main.init)
-                if (L_Status.InvokeRequired) L_Status.Invoke((MethodInvoker)delegate { L_Status.Text = Main.Status; });
-                else { L_Status.Text = Main.Status; }
-            else
-                if (InvokeRequired && IsHandleCreated)
-                    try { Invoke((MethodInvoker)Close); } catch { Close(); }
+            new Thread(() =>
+            {
+                while (!Main.init)
+                    Thread.Sleep(50);
+
+                if (InvokeRequired)
+                    try { Invoke((MethodInvoker)Close); }
+                    catch { Close(); }
                 else Close();
+            }).Start();
         }
     }
 }
