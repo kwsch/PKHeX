edb4928117c52cbbf4b70e822a377d56737ac770
Kaphotics <kaphotics@gmail.com>
2016-08-10 19:32:57 -0700

    Minor cleanup
    Unused usings/references

--- Tests/PKHeX.Tests/PKM/PKMTests.cs
+++ Tests/PKHeX.Tests/PKM/PKMTests.cs
@@ -1,9 +1,5 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace PKHeX.Tests.PKM
 {
--- Tests/PKHeX.Tests/Util/DateUtilTests.cs
+++ Tests/PKHeX.Tests/Util/DateUtilTests.cs
@@ -1,9 +1,4 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace PKHeX.Tests.Util
 {
