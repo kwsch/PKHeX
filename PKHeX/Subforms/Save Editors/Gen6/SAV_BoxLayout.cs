a9648f021bcd5b0b33c1c6cecce60ac90c639f27
Kaphotics <kaphotics@gmail.com>
2016-10-29 11:33:53 -0700

    Generalize box viewer
    Need to update string tables for all games

--- PKHeX/Subforms/Save Editors/Gen6/SAV_BoxLayout.cs
+++ PKHeX/Subforms/Save Editors/Gen6/SAV_BoxLayout.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Windows.Forms;
 
 namespace PKHeX
@@ -17,28 +18,72 @@ namespace PKHeX
                 CB_BG.Items.Add(wallpaper);
             
             // Go
-            MT_BG1.Text = SAV.Data[SAV.PCFlags + 0].ToString();
-            CB_Unlocked.SelectedIndex = SAV.Data[SAV.PCFlags + 1] - 1;
-            MT_BG2.Text = SAV.Data[SAV.PCFlags + 2].ToString();
+            LB_BoxSelect.Items.Clear();
+            for (int i = 0; i < SAV.BoxCount; i++)
+                LB_BoxSelect.Items.Add(SAV.getBoxName(i));
+
+            // Flags
+            byte[] flags = SAV.BoxFlags;
+            if (flags != null)
+            {
+                flagArr = new NumericUpDown[flags.Length];
+                for (int i = 0; i < flags.Length; i++)
+                {
+                    flagArr[i] = new NumericUpDown
+                    {
+                        Minimum = 0,
+                        Maximum = 255,
+                        Width = CB_Unlocked.Width - 5,
+                        Hexadecimal = true,
+                        Value = flags[i]
+                    };
+                    FLP_Flags.Controls.Add(flagArr[i]);
+                }
+            }
+            else
+            {
+                FLP_Flags.Visible = false;
+            }
+
+            // Unlocked
+            if (SAV.BoxesUnlocked > 0)
+            {
+                CB_Unlocked.Items.Clear();
+                for (int i = 0; i < SAV.BoxCount; i++)
+                    CB_Unlocked.Items.Add(i + 1);
+                CB_Unlocked.SelectedIndex = SAV.BoxesUnlocked;
+            }
+            else
+            {
+                FLP_Unlocked.Visible = L_Unlocked.Visible = CB_Unlocked.Visible = false;
+            }
             LB_BoxSelect.SelectedIndex = box;
         }
-        private readonly SAV6 SAV = new SAV6(Main.SAV.Data);
-        private bool editing;
 
+        private readonly NumericUpDown[] flagArr = new NumericUpDown[0];
+        private readonly SaveFile SAV = Main.SAV.Clone();
+        private bool editing;
+        private bool renameBox;
         private void changeBox(object sender, EventArgs e)
         {
+            if (renameBox)
+                return;
             editing = true;
-
-            int bgoff = SAV.PCBackgrounds + LB_BoxSelect.SelectedIndex;
-            CB_BG.SelectedIndex = SAV.Data[bgoff];
+            
+            CB_BG.SelectedIndex = SAV.getBoxWallpaper(LB_BoxSelect.SelectedIndex);
             TB_BoxName.Text = SAV.getBoxName(LB_BoxSelect.SelectedIndex);
 
             editing = false; 
         }
         private void changeBoxDetails(object sender, EventArgs e)
         {
-            if (!editing)
-                SAV.setBoxName(LB_BoxSelect.SelectedIndex, TB_BoxName.Text);
+            if (editing)
+                return;
+
+            renameBox = true;
+            SAV.setBoxName(LB_BoxSelect.SelectedIndex, TB_BoxName.Text);
+            LB_BoxSelect.Items[LB_BoxSelect.SelectedIndex] = TB_BoxName.Text;
+            renameBox = false;
         }
         private void B_Cancel_Click(object sender, EventArgs e)
         {
@@ -46,11 +91,12 @@ namespace PKHeX
         }
         private void B_Save_Click(object sender, EventArgs e)
         {
-            SAV.Data[SAV.PCFlags + 0] = (byte)Util.ToUInt32(MT_BG1.Text);
-            SAV.Data[SAV.PCFlags + 1] = (byte)Util.ToUInt32(CB_Unlocked.Text);
-            SAV.Data[SAV.PCFlags + 2] = (byte)Util.ToUInt32(MT_BG2.Text);
+            if (flagArr.Length > 0)
+                SAV.BoxFlags = flagArr.Select(i => (byte) i.Value).ToArray();
+            if (CB_Unlocked.Visible)
+                SAV.BoxesUnlocked = CB_Unlocked.SelectedIndex;
 
-            Array.Copy(SAV.Data, Main.SAV.Data, SAV.Data.Length);
+            Main.SAV = SAV;
             Main.SAV.Edited = true;
             Close();
         }
@@ -58,7 +104,7 @@ namespace PKHeX
         private void changeBoxBG(object sender, EventArgs e)
         {
             if (!editing)
-                SAV.Data[SAV.PCBackgrounds + LB_BoxSelect.SelectedIndex] = (byte)CB_BG.SelectedIndex;
+                SAV.setBoxWallpaper(LB_BoxSelect.SelectedIndex, CB_BG.SelectedIndex);
 
             PAN_BG.BackgroundImage = BoxWallpaper.getWallpaper(SAV, CB_BG.SelectedIndex);
         }
