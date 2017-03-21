9d781a884acfa014b865a046c04ee7e2970e9d67
wwwwwwzx <togekiss.beyond@gmail.com>
2017-03-20 23:13:11 -0700

    Fix dppt roaming locations
    - roaming pkm can be met on water in dppt
    - can not go to route 223 224 and northwest island 225-230
    - Add 2 non-route location

--- PKHeX/Legality/Tables4.cs
+++ PKHeX/Legality/Tables4.cs
@@ -238,10 +238,12 @@ namespace PKHeX.Core
 
         internal static readonly int[] Roaming_MetLocation_DPPt =
         {
-            // Route 201-230  
+            // Route 201-222 can be encountered in either grass or water
             16, 17, 18, 19, 20, 21, 22, 23, 24, 25,
-            26, 27, 28, 29, 30, 31, 32, 33,
-            36, 37,     39, 40, 41, 42, 43, 44, 45, // Route 219,220,223 surfing only 
+            26, 27, 28, 29, 30, 31, 32, 33, 34, 35,
+            36, 37, 
+            47,     // Valley Windworks
+            49,     // Fuego Ironworks
         };
         internal static readonly EncounterStatic[] Encounter_DPPt_Roam =
         {
