47028028420fb3b61e385141ed2eb7a4f4fa5eb4
wwwwwwzx <togekiss.beyond@gmail.com>
2017-03-22 15:42:59 -0700

    Add gen4/5 egg valid met locations
    -also fix Regis met location in b2w2

--- PKHeX/Legality/Tables5.cs
+++ PKHeX/Legality/Tables5.cs
@@ -232,9 +232,9 @@ namespace PKHeX.Core
             new EncounterStatic { Species = 612, Level = 60, Location = 147, Shiny = true}, // Haxorus @ Nature Preserve
 
             //Stationary Lengerdary
-            new EncounterStatic { Species = 377, Level = 65, Location = 149,}, //Regirock @ Underground Ruins
-            new EncounterStatic { Species = 378, Level = 65, Location = 149,}, //Regice @ Underground Ruins
-            new EncounterStatic { Species = 379, Level = 65, Location = 149,}, //Registeel @ Underground Ruins
+            new EncounterStatic { Species = 377, Level = 65, Location = 150,}, //Regirock @ Rock Peak Chamber
+            new EncounterStatic { Species = 378, Level = 65, Location = 151,}, //Regice @ Iceberg Chamber
+            new EncounterStatic { Species = 379, Level = 65, Location = 152,}, //Registeel @ Iron Chamber
             new EncounterStatic { Species = 380, Level = 68, Location = 032, Version = GameVersion.W2, }, // Latias @ Dreamyard
             new EncounterStatic { Species = 381, Level = 68, Location = 032, Version = GameVersion.B2, }, // Latios @ Dreamyard
             new EncounterStatic { Species = 480, Level = 65, Location = 007,}, //Uxie @ Nacrene City
@@ -486,11 +486,24 @@ namespace PKHeX.Core
 
         internal static readonly int[] ValidMet_BW =
         {
-            // todo
+                           004, 005, 006, 007, 008, 009, 010, 011, 012, 013, 014, 015, 016, 017, 018, 019, 020,
+            021, 022, 023, 024, 025, 026, 027, 028, 029, 030, 031, 032, 033, 034, 035, 036, 037, 038, 039, 040,
+            041, 042, 043, 044, 045, 046, 047, 048, 049, 050, 051, 052, 053, 054, 055, 056, 057, 058, 059, 060,
+            061, 062, 063, 064, 065, 066, 067, 068, 069, 070, 071, 072, 073, 074, 075, 076, 077, 078, 079, 080,
+            081, 082, 083, 084, 085, 086, 087, 088, 089, 090, 091, 092, 093, 094, 095, 096, 097, 098, 099, 100,
+            101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116,
+            
         };
         internal static readonly int[] ValidMet_B2W2 =
         {
-            // todo
+                           004, 005, 006, 007, 008, 009, 010, 011, 012, 013, 014, 015, 016, 017, 018, 019, 020,
+            021, 022,      024, 025, 026, 027, 028, 029, 030, 031, 032, 033, 034, 035, 036, 037, 038, 039,      //023 Route 10, 040->134 Victory Road
+            041, 042, 043, 044, 045, 046, 047, 048, 049, 050, 051, 052, 053, 054, 055, 056, 057, 058,      060, //059 Challenger's cave
+            061, 062, 063, 064, 065, 066, 067, 068, 069, 070, 071, 072, 073, 074, 075, 076, 077, 078, 079, 080,
+            081, 082, 083, 084, 085, 086, 087, 088, 089, 090, 091, 092, 093, 094, 095, 096, 097, 098, 099, 100,
+            101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120,
+            121, 122, 123, 124, 125, 126, 127, 128, 129, 130, 131, 132, 133, 134, 135, 136, 137,      139, 140, //138 ---
+            141, 142, 143, 144, 145, 146, 147, 148, 149, 150, 151, 152, 153,
         };
     }
 }
