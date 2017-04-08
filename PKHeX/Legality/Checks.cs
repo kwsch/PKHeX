8123cb8b120922da23564e43fbfadaad4cb8012f
wwwwwwzx <togekiss.beyond@gmail.com>
2017-04-07 23:08:23 -0700

    Fix B2W2 Tutor Move
    Iron tail (231) is taught at Lentimas city. Also fix the string ID of
    Tutor Move

--- PKHeX/Legality/Checks.cs
+++ PKHeX/Legality/Checks.cs
@@ -2646,7 +2646,7 @@ namespace PKHeX.Core
                     else if (tmhm[gen].Contains(moves[m]))
                         res[m] = new CheckResult(Severity.Valid, native ? V173 : string.Format(V331, gen), CheckIdentifier.Move);
                     else if (tutor[gen].Contains(moves[m]))
-                        res[m] = new CheckResult(Severity.Valid, native ? V173 : string.Format(V332, gen), CheckIdentifier.Move);
+                        res[m] = new CheckResult(Severity.Valid, native ? V174 : string.Format(V332, gen), CheckIdentifier.Move);
                     else if (gen == pkm.GenNumber && special.Contains(moves[m]))
                         res[m] = new CheckResult(Severity.Valid, V175, CheckIdentifier.Move);
 
