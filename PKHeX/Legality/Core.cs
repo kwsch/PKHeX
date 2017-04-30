1ff2a0fdfa46b147ceb5bbcf7a0d260672bf8a1f
Kurt <kaphotics@gmail.com>
2017-04-30 12:17:27 -0700

    Add preliminary CXD checking
    quite a few shadow pkm are obtainable in multiple locations, feeling
    like duplicating the entries with different location IDs is the way to
    go

--- PKHeX/Legality/Core.cs
+++ PKHeX/Legality/Core.cs
@@ -103,6 +103,9 @@ namespace PKHeX.Core
                 case GameVersion.FR: case GameVersion.LG:
                     table = Encounter_FRLG;
                     break;
+                case GameVersion.CXD:
+                    table = Encounter_CXD;
+                    break;
 
                 case GameVersion.D: case GameVersion.P: case GameVersion.Pt:
                     table = Encounter_DPPt;
@@ -2819,6 +2822,8 @@ namespace PKHeX.Core
                     return getSlots(pkm, SlotsFR, lvl);
                 case GameVersion.LG:
                     return getSlots(pkm, SlotsLG, lvl);
+                case GameVersion.CXD:
+                    return getSlots(pkm, SlotsXD, lvl);
 
                 case GameVersion.D:
                     return getSlots(pkm, SlotsD, lvl);
@@ -2883,6 +2888,8 @@ namespace PKHeX.Core
                     return getStatic(pkm, StaticFR, lvl);
                 case GameVersion.LG:
                     return getStatic(pkm, StaticLG, lvl);
+                case GameVersion.CXD:
+                    return getStatic(pkm, Encounter_CXD, lvl);
 
                 case GameVersion.D:
                     return getStatic(pkm, StaticD, lvl);
