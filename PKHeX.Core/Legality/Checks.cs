5f94a2e0f4825f998494b0647af2d14412405dca
Kurt <kaphotics@gmail.com>
2017-05-23 20:10:57 -0700

    Update encountertrade ability legality check
    check for encountertrade edge case first, then ability capsule
    
    Closes #1163

--- PKHeX.Core/Legality/Checks.cs
+++ PKHeX.Core/Legality/Checks.cs
@@ -1458,11 +1458,10 @@ namespace PKHeX.Core
             if (!(AbilityUnchanged ?? false) || EncounterAbility == 0 || pkm.AbilityNumber == EncounterAbility)
                 return false;
 
-            if (pkm.Format >= 6 && abilities[0] != abilities[1] && pkm.AbilityNumber < 4) // Ability Capsule
-                AddLine(Severity.Valid, V109, CheckIdentifier.Ability);
-            else if (pkm.Gen3 && EncounterMatch is EncounterTrade && EncounterAbility == 1 << abilval)
-                // Edge case (Static PID?)
+            if (EncounterMatch is EncounterTrade z && EncounterAbility == 1 << abilval && z.Species == pkm.Species) // Edge case (Static PID?)
                 AddLine(Severity.Valid, V115, CheckIdentifier.Ability);
+            else if (pkm.Format >= 6 && abilities[0] != abilities[1] && pkm.AbilityNumber < 4) // Ability Capsule
+                AddLine(Severity.Valid, V109, CheckIdentifier.Ability);
             else
                 AddLine(Severity.Invalid, V223, CheckIdentifier.Ability);
             return true;
