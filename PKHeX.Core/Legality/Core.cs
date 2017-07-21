da4a9f9c5b1b063d1daa42102e5260744d591581
architdate <architdate@gmail.com>
2017-07-21 11:36:32 +0800

    Fixed Zygarde Legalities from Gen 6 origin

--- PKHeX.Core/Legality/Core.cs
+++ PKHeX.Core/Legality/Core.cs
@@ -2085,8 +2085,11 @@ namespace PKHeX.Core
                 return true;
             if (IsEvolvedFormChange(pkm))
                 return true;
-            if (pkm.Species == 718 && pkm.InhabitedGeneration(7) && pkm.AltForm == 3)
+            if (pkm.Species == 718 && pkm.InhabitedGeneration(7))
+            {
+                if (pkm.AltForm == 1) return false;
                 return true;
+            }
             return false;
         }
         
