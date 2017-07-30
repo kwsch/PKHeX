c15cfdc52dd580c58c3982936406425e1f33956d
Kurt <kaphotics@gmail.com>
2017-07-30 15:13:45 -0700

    Fix b2w2 reference
    yay copypaste
    Closes #1373

--- PKHeX.Core/Legality/Encounters/Data/Encounters5.cs
+++ PKHeX.Core/Legality/Encounters/Data/Encounters5.cs
@@ -14,7 +14,7 @@ namespace PKHeX.Core
             MarkG5DreamWorld(BW_DreamWorld);
             MarkG5DreamWorld(B2W2_DreamWorld);
             var staticbw = Encounter_BW.Concat(BW_DreamWorld).ToArray();
-            var staticb2w2 = Encounter_BW.Concat(BW_DreamWorld).ToArray();
+            var staticb2w2 = Encounter_B2W2.Concat(B2W2_DreamWorld).ToArray();
             StaticB = GetStaticEncounters(staticbw, GameVersion.B);
             StaticW = GetStaticEncounters(staticbw, GameVersion.W);
             StaticB2 = GetStaticEncounters(staticb2w2, GameVersion.B2);
