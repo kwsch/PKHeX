a679312371b9cfca22d9c5541ddeecf373434ed8
Kurt <kaphotics@gmail.com>
2017-08-30 21:30:41 -0700

    Change box/party illegal flagging default setting
    i think it's time to turn this on by default for new settings

--- PKHeX.WinForms/Properties/Settings.Designer.cs
+++ PKHeX.WinForms/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace PKHeX.WinForms.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "14.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.3.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -97,7 +97,7 @@ namespace PKHeX.WinForms.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("False")]
+        [global::System.Configuration.DefaultSettingValueAttribute("True")]
         public bool FlagIllegal {
             get {
                 return ((bool)(this["FlagIllegal"]));
