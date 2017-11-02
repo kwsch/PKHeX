e0673ea6fc093dc82f6a1c7284448b5d0fd7da8e
Evan Dixon <evanldixon@live.com>
2017-11-01 21:16:14 -0500

    Add basic test for pokemon name retreival

---
+++ Tests/PKHeX.Tests/Util/DataUtilTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PKHeX.Tests.Util
+{
+    [TestClass]
+    public class DataUtilTests
+    {
+        const string TestCategory = "Data Util Tests";
+
+        [TestMethod]
+        [TestCategory(TestCategory)]
+        public void TestGetPokemonNames()
+        {
+            var names = PKHeX.Core.Util.GetSpeciesList("en");
+            Assert.AreEqual(803, names.Length);
+        }
+    }
+}
