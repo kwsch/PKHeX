52a22fbf7d2f24b15fe83c7b00e866084d894e68
Evan Dixon <evanldixon@live.com>
2017-11-07 19:21:17 -0600

    Add more checks to failing test

--- Tests/PKHeX.Tests/Saves/SMTests.cs
+++ Tests/PKHeX.Tests/Saves/SMTests.cs
@@ -30,9 +30,12 @@ namespace PKHeX.Tests.Saves
         public void TestChecksumUpdate()
         {
             var save = GetSave();
+            var saveChecksumInfo = save.ChecksumInfo;
             var newSave = new SAV7(save.Write(false, false));
-            Assert.IsTrue(newSave.ChecksumsValid, "Checksums are not valid.");
-            Assert.AreEqual(save.ChecksumInfo, newSave.ChecksumInfo, "Checksums changed since saving without modification.");
+            Assert.AreEqual(saveChecksumInfo, save.ChecksumInfo, "Checksum info modified on Write");
+            Assert.IsTrue(save.ChecksumsValid, "Checksum not valid after write");
+            Assert.IsTrue(newSave.ChecksumsValid, "Checksums are not valid after open");
+            Assert.AreEqual(save.ChecksumInfo, newSave.ChecksumInfo, "Checksums changed since write and open");
         }
     }
 }
