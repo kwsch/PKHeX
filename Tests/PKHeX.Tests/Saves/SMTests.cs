bb02488c6d31a92e1b06c3f8a319f861cecb6cc7
Evan Dixon <evanldixon@live.com>
2017-11-07 19:07:13 -0600

    Add checksum tests

---
+++ Tests/PKHeX.Tests/Saves/SMTests.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PKHeX.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PKHeX.Tests.Saves
+{
+    [TestClass]
+    public class SMTests
+    {
+        public const string TestCategory = "SM Save Data Tests";
+
+        private SAV7 GetSave()
+        {
+            return new SAV7(Properties.Resources.SM_Project_802);
+        }
+
+        [TestMethod]
+        [TestCategory(TestCategory)]
+        public void TestChecksumRead()
+        {
+            Assert.IsTrue(GetSave().ChecksumsValid, "Checksums are not valid.");
+        }
+
+        [TestMethod]
+        [TestCategory(TestCategory)]
+        public void TestChecksumUpdate()
+        {
+            var save = GetSave();
+            var newSave = new SAV7(save.Write(false, false));
+            Assert.IsTrue(newSave.ChecksumsValid, "Checksums are not valid.");
+            Assert.AreEqual(save.ChecksumInfo, newSave.ChecksumInfo, "Checksums changed since saving without modification.");
+        }
+    }
+}
