8f5fbeb5041bd39213747befe25e0de94c8a463b
Kurt <kaphotics@gmail.com>
2017-11-19 19:48:45 -0800

    allow pupitar hidden ability on 2->7
    yay

--- PKHeX.Core/Legality/Tables2.cs
+++ PKHeX.Core/Legality/Tables2.cs
@@ -58,6 +58,6 @@ namespace PKHeX.Core
             // todo
         };
         internal static readonly bool[] ReleasedHeldItems_2 = Enumerable.Range(0, MaxItemID_2+1).Select(i => HeldItems_GSC.Contains((ushort)i) && !UnreleasedItems_2.Contains(i)).ToArray();
-        internal static readonly HashSet<int> TransferSpeciesDefaultAbility_2 = new HashSet<int> { 92, 93, 94, 109, 110, 151, 200, 201, 247, 251}; // todo VC2: 247 (pupitar)
+        internal static readonly HashSet<int> TransferSpeciesDefaultAbility_2 = new HashSet<int> { 92, 93, 94, 109, 110, 151, 200, 201, 251 };
     }
 }
