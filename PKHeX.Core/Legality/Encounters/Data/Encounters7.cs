0bc2ee0938ebb5c652e7b038f0694991e89d5ef8
Kurt <kaphotics@gmail.com>
2017-11-28 19:29:22 -0800

    Skip scatterbug static encounter forme check
    closes #1668
    add 2/3 pkms provided as test cases (spewpa can be ignored, close
    enough. just want an evo & orig).

--- PKHeX.Core/Legality/Encounters/Data/Encounters7.cs
+++ PKHeX.Core/Legality/Encounters/Data/Encounters7.cs
@@ -234,7 +234,7 @@ namespace PKHeX.Core
             new EncounterStatic { Species = 007, Level = 12, Location = 042, Relearn = new[] {453,110,055,033}, }, // Squirtle @ Seaward Cave
             new EncounterStatic { Species = 095, Level = 14, Location = 034, Relearn = new[] {563,099,317,088}, }, // Onix @ Ten Carat Hill
             new EncounterStatic { Species = 116, Level = 18, Location = 014, Relearn = new[] {352,239,055,043}, }, // Horsea @ Kala'e Bay
-            new EncounterStatic { Species = 664, Level = 09, Location = 020, Relearn = new[] {476,081,078,033}, }, // Scatterbug @ Hau'oli City
+            new EncounterStatic { Species = 664, Level = 09, Location = 020, Relearn = new[] {476,081,078,033}, SkipFormCheck = true, }, // Scatterbug @ Hau'oli City
             new EncounterStatic { Species = 001, Level = 10, Location = 012, Relearn = new[] {580,022,045,033}, }, // Bulbasaur @ Route 2
             new EncounterStatic { Species = 607, Level = 09, Location = 038, Relearn = new[] {203,052,083,123}, }, // Litwick @ Hau'oli Cemetery
 			
