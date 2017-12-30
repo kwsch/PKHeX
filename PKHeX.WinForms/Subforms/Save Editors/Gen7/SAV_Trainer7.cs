fa90ae21dfe7258a6ca4d0366b9d9522601f3dc7
Kurt <kaphotics@gmail.com>
2017-12-29 18:30:13 -0800

    Fix variable reference
    #1731

--- PKHeX.WinForms/Subforms/Save Editors/Gen7/SAV_Trainer7.cs
+++ PKHeX.WinForms/Subforms/Save Editors/Gen7/SAV_Trainer7.cs
@@ -475,7 +475,7 @@ namespace PKHeX.WinForms
             SAV.SetSurfScore(3, (int)NUD_Surf3.Value);
 
             if (TB_RotomOT.Text != TB_OTName.Text // different Rotom name from OT
-                && TB_RotomOT.Text != SAV.OT // manually changed
+                && TB_OTName.Text != SAV.OT // manually changed
                 && DialogResult.Yes == // wants to update
                 WinFormsUtil.Prompt(MessageBoxButtons.YesNo, "Rotom OT does not match OT name. Update Rotom OT name with OT name?"))
                 SAV.RotomOT = TB_OTName.Text;
