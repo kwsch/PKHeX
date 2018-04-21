0b62ab85a1f0c94044983abc8422a7dbc47be5ed
Kurt <kaphotics@gmail.com>
2018-04-21 09:18:28 -0700

    Nature -> byte instead of int

--- PKHeX.Core/Legality/Structures/Nature.cs
+++ PKHeX.Core/Legality/Structures/Nature.cs
@@ -3,13 +3,14 @@
     /// <summary>
     /// Nature ID values for the corresponding English nature name.
     /// </summary>
-    public enum Nature
+    public enum Nature : byte
     {
-        Random = -1,
         Hardy, Lonely, Brave, Adamant, Naughty, Bold,
         Docile, Relaxed, Impish, Lax, Timid, Hasty,
         Serious, Jolly, Naive, Modest, Mild, Quiet,
         Bashful, Rash, Calm, Gentle, Sassy, Careful,
         Quirky,
+
+        Random = 25,
     }
 }
