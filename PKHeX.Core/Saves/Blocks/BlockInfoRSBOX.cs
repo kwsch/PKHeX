4778c9c75883bab5fb07a499cec5e8c17340bdf9
Kurt <kaphotics@gmail.com>
2018-06-11 09:12:41 -0700

    Fix RSBOX checksum
    Closes #1995
    clean up variable names

--- PKHeX.Core/Saves/Blocks/BlockInfoRSBOX.cs
+++ PKHeX.Core/Saves/Blocks/BlockInfoRSBOX.cs
@@ -25,7 +25,7 @@ namespace PKHeX.Core
 
         protected override void SetChecksum(byte[] data)
         {
-            var chk = GetChecksum(data, Offset, Length);
+            var chk = GetChecksum(data);
             data[Offset + 0] = (byte)(chk >> 24);
             data[Offset + 1] = (byte)(chk >> 16);
             data[Offset + 2] = (byte)(chk >> 8);
@@ -35,15 +35,15 @@ namespace PKHeX.Core
         private uint GetChecksum(byte[] data)
         {
             int start = Offset + 4;
-            int length = start + Length - 4;
+            int end = start + Length - 4;
 
-            return GetChecksum(data, start, length);
+            return GetChecksum(data, start, end);
         }
 
-        private static uint GetChecksum(byte[] data, int start, int length)
+        private static uint GetChecksum(byte[] data, int start, int end)
         {
             ushort chk = 0; // initial value
-            for (int j = start; j < length; j += 2)
+            for (int j = start; j < end; j += 2)
                 chk += BigEndian.ToUInt16(data, j);
             return (uint)(chk << 16 | (ushort)(0xF004 - chk));
         }
