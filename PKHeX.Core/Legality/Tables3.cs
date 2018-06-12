c3314f1b8ee173349428c9ccb070d418302a5e61
Kurt <kaphotics@gmail.com>
2018-06-12 16:41:44 -0700

    Remove Mirage Island met location from valid gen3
    Although Pokémon Ruby, Sapphire, and Emerald include a location pointer
    for "Mirage Island", any Wynaut caught here will simply display "Met on
    Route 130" in their summary. This is because the player is technically
    still on Route 130's map.
    
    Can't get anything with that location ID as an egg or as wild

--- PKHeX.Core/Legality/Tables3.cs
+++ PKHeX.Core/Legality/Tables3.cs
@@ -236,14 +236,15 @@ namespace PKHeX.Core
             253, //Ingame egg gift
             255 // event/pokemon box
         };
-        // 064 is an unused location for metor falls
+        // 064 is an unused location for Meteor Falls
         // 084 is Inside of a truck, no possible pokemon can be hatched there
+        // 071 is Mirage island, cannot be obtained as the player is technically still on Route 130's map.
         internal static readonly HashSet<int> ValidMet_RS = new HashSet<int>
         {
             000, 001, 002, 003, 004, 005, 006, 007, 008, 009, 010, 011, 012, 013, 014, 015, 016, 017, 018, 019,
             020, 021, 022, 023, 024, 025, 026, 027, 028, 029, 030, 031, 032, 033, 034, 035, 036, 037, 038, 039,
             040, 041, 042, 043, 044, 045, 046, 047, 048, 049, 050, 051, 052, 053, 054, 055, 056, 057, 058, 059,
-            060, 061, 062, 063, 065, 066, 067, 068, 069, 070, 071, 072, 073, 074, 075, 076, 077, 078, 079, 080,
+            060, 061, 062, 063, 065, 066, 067, 068, 069, 070     , 072, 073, 074, 075, 076, 077, 078, 079, 080,
             081, 082, 083, 085, 086, 087,
         };
         // 155 - 158 Sevii Isle 6-9 Unused
