0c2d3029975423b41ffdff1b175fc868e47afbf6
Kurt <kaphotics@gmail.com>
2018-07-05 16:57:29 -0700

    Show tid7/sid7 for gen7+ origin
    https://projectpokemon.org/home/forums/topic/46050-box-data-report-doesnt-display-correct-gen-7-tids/

--- PKHeX.WinForms/Subforms/ReportGrid.cs
+++ PKHeX.WinForms/Subforms/ReportGrid.cs
@@ -90,8 +90,8 @@ namespace PKHeX.WinForms
             public bool IsNicknamed => pkm.IsNicknamed;
             public bool IsShiny => pkm.IsShiny;
 
-            public int TID => pkm.TID;
-            public int SID => pkm.SID;
+            public int TID => pkm.GenNumber >= 7 ? pkm.TrainerID7 : pkm.TID;
+            public int SID => pkm.GenNumber >= 7 ? pkm.TrainerSID7 : pkm.SID;
             public int TSV => pkm.TSV;
             public int Move1_PP => pkm.Move1_PP;
             public int Move2_PP => pkm.Move2_PP;
