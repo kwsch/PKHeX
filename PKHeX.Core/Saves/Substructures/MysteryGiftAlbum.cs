0de335d52bb5bb85bf2763c0e44800f81c0b16dd
Kurt <kaphotics@gmail.com>
2018-07-29 14:04:16 -0700

    Misc reorg

---
+++ PKHeX.Core/Saves/Substructures/MysteryGiftAlbum.cs
@@ -0,0 +1,12 @@
+﻿namespace PKHeX.Core
+{
+    /// <summary>
+    /// Structure containing Mystery Gift Block Data
+    /// </summary>
+    public class MysteryGiftAlbum
+    {
+        public MysteryGift[] Gifts;
+        public bool[] Flags;
+        public uint Seed;
+    }
+}
