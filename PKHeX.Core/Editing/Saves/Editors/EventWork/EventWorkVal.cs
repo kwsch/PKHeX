f62e3f43b396546fa6f484b1c45ae3b9665514ba
Kurt <kaphotics@gmail.com>
2018-11-13 19:18:04 -0800

    Add eventwork editing abstractions

---
+++ PKHeX.Core/Editing/Saves/Editors/EventWork/EventWorkVal.cs
@@ -0,0 +1,25 @@
+﻿namespace PKHeX.Core
+{
+    /// <summary>
+    /// Represents a known value for a <see cref="EventWork{T}"/> of type <see cref="int"/>.
+    /// </summary>
+    public class EventWorkVal
+    {
+        public readonly bool Custom;
+        public readonly string Text;
+        public readonly int Value;
+
+        public EventWorkVal()
+        {
+            Custom = true;
+            Text = nameof(Custom);
+            Value = int.MinValue;
+        }
+
+        public EventWorkVal(string text, int val)
+        {
+            Text = text;
+            Value = val;
+        }
+    }
+}
---
+++ PKHeX.Core/Editing/Saves/Editors/EventWork/SplitEventEditor.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace PKHeX.Core
+{
+    /// <summary>
+    /// Editor object that unpacks <see cref="EventWork{T}"/> into flags & work groups, and handles value get/set operations.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class SplitEventEditor<T>
+    {
+        public readonly IList<EventVarGroup> Work;
+        public readonly IList<EventVarGroup> Flag;
+        public readonly IEventWork<T> Block;
+
+        public SplitEventEditor(IEventWork<T> block, IEnumerable<string> work, IEnumerable<string> flag)
+        {
+            Block = block;
+            // load lines
+            var worklines = work.Where(z => !string.IsNullOrWhiteSpace(z) && z.Length > 5);
+            Work = EventWorkUtil.GetVars(worklines, (index, t, data) => new EventWork<T>(index, t, data));
+            var flaglines = flag.Where(z => !string.IsNullOrWhiteSpace(z) && z.Length > 5);
+            Flag = EventWorkUtil.GetVars(flaglines, (index, t, data) => new EventFlag(index, t, data));
+
+            // initialize lines
+            foreach (var g in Work)
+            {
+                foreach (var item in g.Vars)
+                {
+                    item.RawIndex = block.GetWorkRawIndex(item.Type, item.RelativeIndex);
+                    ((EventWork<T>)item).Value = block.GetWork(item.RawIndex);
+                }
+            }
+            foreach (var g in Flag)
+            {
+                foreach (var item in g.Vars)
+                {
+                    item.RawIndex = block.GetFlagRawIndex(item.Type, item.RelativeIndex);
+                    ((EventFlag)item).Flag = block.GetFlag(item.RawIndex);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Writes all of the updated event values back to the block.
+        /// </summary>
+        public void Save()
+        {
+            foreach (var g in Work)
+            {
+                foreach (var item in g.Vars)
+                {
+                    var val = ((EventWork<T>)item).Value;
+                    Block.SetWork(item.RawIndex, val);
+                }
+            }
+            foreach (var g in Flag)
+            {
+                foreach (var item in g.Vars)
+                {
+                    var val = ((EventFlag)item).Flag;
+                    Block.SetFlag(item.RawIndex, val);
+                }
+            }
+        }
+    }
+}
