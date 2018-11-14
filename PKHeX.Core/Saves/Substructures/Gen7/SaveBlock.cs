025a290bc98136e7716cc6631b3cd163c92bc966
Kurt <kaphotics@gmail.com>
2018-11-13 19:15:27 -0800

    Update saveblock template for stored offset fetch

--- PKHeX.Core/Saves/Substructures/Gen7/SaveBlock.cs
+++ PKHeX.Core/Saves/Substructures/Gen7/SaveBlock.cs
@@ -2,6 +2,7 @@ namespace PKHeX.Core
 {
     public abstract class SaveBlock
     {
+        public int Offset { get; protected set; }
         protected readonly byte[] Data;
         protected SaveBlock(SaveFile SAV) => Data = SAV.Data;
     }
