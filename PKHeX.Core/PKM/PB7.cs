6a0a6aa82aea7b61a6ceac9e8614abdbf537f69c
Kurt <kaphotics@gmail.com>
2018-11-14 22:51:53 -0800

    Add methods for 7b quirks
    nidoran m/f when sanitized with the old way show the gender symbols as
    '?'

--- PKHeX.Core/PKM/PB7.cs
+++ PKHeX.Core/PKM/PB7.cs
@@ -33,7 +33,7 @@ namespace PKHeX.Core
         public override PKM Clone() => new PB7((byte[])Data.Clone(), Identifier);
 
         private string GetString(int Offset, int Count) => StringConverter.GetString7(Data, Offset, Count);
-        private byte[] SetString(string value, int maxLength, bool chinese = false) => StringConverter.SetString7(value, maxLength, Language, chinese: chinese);
+        private byte[] SetString(string value, int maxLength, bool chinese = false) => StringConverter.SetString7b(value, maxLength, Language, chinese: chinese);
 
         protected override ushort CalculateChecksum()
         {
