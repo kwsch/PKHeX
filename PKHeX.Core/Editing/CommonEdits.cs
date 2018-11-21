0cc82edc48e721286f61546b7fd53071e72825c7
Kurt <kaphotics@gmail.com>
2018-11-20 23:57:38 -0800

    Reset CP on import

--- PKHeX.Core/Editing/CommonEdits.cs
+++ PKHeX.Core/Editing/CommonEdits.cs
@@ -317,10 +317,11 @@ namespace PKHeX.Core
             if (pk is IAwakened a)
             {
                 a.SetSuggestedAwakenedValues(pk);
-                if (pk is PB7)
+                if (pk is PB7 b)
                 {
                     for (int i = 0; i < 6; i++)
                         pk.SetEV(i, 0);
+                    b.ResetCalculatedValues();
                 }
             }
 
