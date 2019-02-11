304203262dcc915ff16f98e7e2286085d0b750e2
Kurt <kaphotics@gmail.com>
2019-02-10 21:28:32 -0800

    Add species & ability enumerations
    remove hardcoded 810 in unit test, add ability test too

--- Tests/PKHeX.Core.Tests/Util/DataUtilTests.cs
+++ Tests/PKHeX.Core.Tests/Util/DataUtilTests.cs
@@ -1,4 +1,5 @@
 ﻿using FluentAssertions;
+using PKHeX.Core;
 using Xunit;
 
 namespace PKHeX.Tests.Util
@@ -6,10 +7,17 @@ namespace PKHeX.Tests.Util
     public class DataUtilTests
     {
         [Fact]
-        public void GetsCorrectNumberOfPokemonNames()
+        public void GetsCorrectNumberOfSpeciesNames()
         {
             var names = Core.Util.GetSpeciesList("en");
-            names.Length.Should().Be(810);
+            names.Length.Should().Be((int)Species.MAX_COUNT);
+        }
+
+        [Fact]
+        public void GetsCorrectNumberOfAbilityNames()
+        {
+            var names = Core.Util.GetAbilitiesList("en");
+            names.Length.Should().Be((int)Ability.MAX_COUNT);
         }
     }
 }
