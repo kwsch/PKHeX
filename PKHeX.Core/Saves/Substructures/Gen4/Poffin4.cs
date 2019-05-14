091b9e22817d1a643565ae29dc69f4e2de161ec6
Kurt <kaphotics@gmail.com>
2019-05-13 22:56:45 -0700

    Add g4 poffin editor (misc edits)
    Thanks @sora10pls for docs!
    https://pastebin.com/jNeVJQi0

--- PKHeX.Core/Saves/Substructures/Gen4/Poffin4.cs
+++ PKHeX.Core/Saves/Substructures/Gen4/Poffin4.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel;
 
 namespace PKHeX.Core
 {
@@ -10,23 +11,37 @@ namespace PKHeX.Core
         public Poffin4(byte[] data, int offset)
         {
             Data = new byte[SIZE];
-            Array.Copy(data, offset, data, 0, SIZE);
+            Array.Copy(data, offset, Data, 0, SIZE);
         }
+
+        private const string Stats = nameof(Stats);
 
         public Poffin4(byte[] data) => Data = data;
 
         public PoffinFlavor4 Type{ get => (PoffinFlavor4)Data[0]; set => Data[0] = (byte)value; }
+
+        [Category(Stats), Description("Cool Stat Boost")]
         public byte BoostSpicy  { get => Data[1]; set => Data[1] = value; }
+
+        [Category(Stats), Description("Beauty Stat Boost")]
         public byte BoostDry    { get => Data[2]; set => Data[2] = value; }
+
+        [Category(Stats), Description("Cute Stat Boost")]
         public byte BoostSweet  { get => Data[3]; set => Data[3] = value; }
+
+        [Category(Stats), Description("Smart/Clever Stat Boost")]
         public byte BoostBitter { get => Data[4]; set => Data[4] = value; }
+
+        [Category(Stats), Description("Tough Stat Boost")]
         public byte BoostSour   { get => Data[5]; set => Data[5] = value; }
+
+        [Category(Stats), Description("Sheen Stat Boost")]
         public byte Smoothness  { get => Data[6]; set => Data[6] = value; }
         // public byte Unused   { get => Data[7]; set => Data[7] = value; }
 
         public bool IsManyStat => Type >= PoffinFlavor4.Rich;
-        public int StatPrimary => IsManyStat ? -1 : (byte)Type / 5;
-        public int StatSecondary => IsManyStat ? -1 : (byte)Type % 5;
+        public PoffinFlavor4 StatPrimary => IsManyStat ? PoffinFlavor4.None : (PoffinFlavor4)(((byte) Type / 5) * 6);
+        public PoffinFlavor4 StatSecondary => IsManyStat ? PoffinFlavor4.None : (PoffinFlavor4)(((byte)Type % 5) * 6);
 
         public void SetAll(byte value = 255, PoffinFlavor4 type = PoffinFlavor4.Rich)
         {
