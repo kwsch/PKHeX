9be5bd1abc741d86a9b4dd3558a1526369107aa9
Kurt <kaphotics@gmail.com>
2019-06-26 20:19:03 -0700

    Move generator test to separate file

---
+++ Tests/PKHeX.Core.Tests/Simulator/GeneratorTests.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using FluentAssertions;
+using PKHeX.Core;
+using Xunit;
+
+namespace PKHeX.Tests.Simulator
+{
+    public class GeneratorTests
+    {
+        public static IEnumerable<object[]> PokemonGenerationTestData()
+        {
+            for (int i = 1; i <= 807; i++)
+            {
+                yield return new object[] { i };
+            }
+        }
+
+        [Theory(Skip = "Feature not ready yet")]
+        [MemberData(nameof(PokemonGenerationTestData))]
+        public void PokemonGenerationReturnsLegalPokemon(int species)
+        {
+            int count = 0;
+            var tr = new SimpleTrainerInfo();
+
+            var pk = new PK7 { Species = species };
+            pk.Gender = pk.GetSaneGender();
+            var ez = EncounterMovesetGenerator.GeneratePKMs(pk, tr);
+            foreach (var e in ez)
+            {
+                var la = new LegalityAnalysis(e);
+                la.Valid.Should().BeTrue($"Because generated Pokemon {count} for {species:000} should be valid");
+                Assert.True(la.Valid);
+                count++;
+            }
+        }
+    }
+}
--- Tests/PKHeX.Core.Tests/Simulator/ShowdownSetTests.cs
+++ Tests/PKHeX.Core.Tests/Simulator/ShowdownSetTests.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using FluentAssertions;
 using PKHeX.Core;
 using Xunit;
 
@@ -157,33 +155,6 @@ namespace PKHeX.Tests.Simulator
 
             sets = ShowdownSet.GetShowdownSets(new [] {"", "   ", " "});
             Assert.True(!sets.Any());
-        }
-
-        public static IEnumerable<object[]> PokemonGenerationTestData()
-        {
-            for (int i = 1; i <= 807; i++)
-            {
-                yield return new object[] { i };
-            }
-        }
-
-        [Theory(Skip = "Feature not ready yet")]
-        [MemberData(nameof(PokemonGenerationTestData))]
-        public void PokemonGenerationReturnsLegalPokemon(int species)
-        {
-            int count = 0;
-            var tr = new SimpleTrainerInfo();
-
-            var pk = new PK7 { Species = species };
-            pk.Gender = pk.GetSaneGender();
-            var ez = EncounterMovesetGenerator.GeneratePKMs(pk, tr);
-            foreach (var e in ez)
-            {
-                var la = new LegalityAnalysis(e);
-                la.Valid.Should().BeTrue($"Because generated Pokemon {count} for {species:000} should be valid");
-                Assert.True(la.Valid);
-                count++;
-            }
         }
 
         private const string SetROCKSMetang =
