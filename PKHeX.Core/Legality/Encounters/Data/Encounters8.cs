524ceb7bf9a4a0e3e835a73cc28a120c7c8587cb
Kurt <kaphotics@gmail.com>
2019-11-16 15:02:32 -0800

    Fix dracozolt species ID
    heh dunno how that happened

--- PKHeX.Core/Legality/Encounters/Data/Encounters8.cs
+++ PKHeX.Core/Legality/Encounters/Data/Encounters8.cs
@@ -42,7 +42,7 @@ namespace PKHeX.Core
             new EncounterStatic { Gift = true, Species = 772, Shiny = Never, Level = 50, Location = 158, FlawlessIVCount = 3, }, // Type: Null
             new EncounterStatic { Gift = true, Species = 848, Shiny = Never, Level = 01, Location = 040, IVs = new[]{-1,31,-1,-1,31,-1}, Ball = 11 }, // Toxel, Attack flawless
 
-            new EncounterStatic { Gift = true, Species = 881, FlawlessIVCount = 3, Level = 10, Location = 068, }, // Dracozolt @ Route 6
+            new EncounterStatic { Gift = true, Species = 880, FlawlessIVCount = 3, Level = 10, Location = 068, }, // Dracozolt @ Route 6
             new EncounterStatic { Gift = true, Species = 881, FlawlessIVCount = 3, Level = 10, Location = 068, }, // Arctozolt @ Route 6
             new EncounterStatic { Gift = true, Species = 882, FlawlessIVCount = 3, Level = 10, Location = 068, }, // Dracovish @ Route 6
             new EncounterStatic { Gift = true, Species = 883, FlawlessIVCount = 3, Level = 10, Location = 068, }, // Arctovish @ Route 6
