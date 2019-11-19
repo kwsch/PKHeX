dd2d7d7bc8e8e8bcc34709fb70b28bc31eda1921
ReignOfComputer <roc@reignofcomputer.com>
2019-11-19 15:14:51 +0800

    Stub Method for Connecting Areas Check

--- PKHeX.Core/Legality/Areas/EncounterArea8.cs
+++ PKHeX.Core/Legality/Areas/EncounterArea8.cs
@@ -30,5 +30,89 @@ namespace PKHeX.Core
         protected override IEnumerable<EncounterSlot> GetFilteredSlots(PKM pkm, IEnumerable<EncounterSlot> slots, int minLevel) => slots;
 
         public static bool IsWildArea8(int loc) => 120 <= loc && loc <= 154;
+
+        public static int[] ConnectingArea8(int loc)
+        {
+            if (!IsWildArea8(loc))
+            {
+                return new int[] { }; // Empty
+            }
+
+            switch (loc)
+            {
+                case 122: // Rolling Fields
+                    // Dappled Grove, East Lake Axewell, West Lake Axewell
+                    // Also connects to South Lake Miloch but too much of a stretch
+                    return new int[] { 124, 128, 130 };
+
+                case 124: // Dappled Grove
+                    // Dappled Grove, Watchtower Ruins
+                    return new int[] { 122, 126 };
+
+                case 126: // Watchtower Ruins
+                    // Dappled Grove, West Lake Axewell
+                    return new int[] { 124, 130 };
+
+                case 128: // East Lake Axewell
+                    // Rolling Fields, West Lake Axewell, Axew's Eye, North Lake Miloch
+                    return new int[] { 122, 130, 132, 138 };
+
+                case 130: // West Lake Axewell
+                    // Rolling Fields, Watchtower Ruins, East Lake Axewell, Axew's Eye
+                    return new int[] { 122, 126, 128, 132 };
+
+                case 132: // Axew's Eye
+                    // East Lake Axewell, West Lake Axewell
+                    return new int[] { 128, 130 };
+
+                case 134: // South Lake Miloch
+                    // Giant's Seat, North Lake Miloch
+                    return new int[] { 136, 138 };
+
+                case 136: // Giant's Seat
+                    // South Lake Miloch, North Lake Miloch
+                    return new int[] { 134, 138 };
+
+                case 138: // North Lake Miloch
+                    // East Lake Axewell, South Lake Miloch, Giant's Seat
+                    // Also connects to Motostoke Riverbank but too much of a stretch
+                    return new int[] { 128, 134, 136 };
+
+                case 140: // Motostoke Riverbank
+                    // Bridge Field
+                    return new int[] { 142 };
+
+                case 142: // Bridge Field
+                    // Motostoke Riverbank, Stony Wilderness
+                    return new int[] { 140, 144 };
+
+                case 144: // Stony Wilderness
+                    // Bridge Field, Dusty Bowl, Giant's Mirror, Giant's Cap
+                    return new int[] { 142, 146, 148, 152 };
+
+                case 146: // Dusty Bowl
+                    // Stony Wilderness, Giant's Mirror, Hammerlocke Hills
+                    return new int[] { 144, 148, 150 };
+
+                case 148: // Giant's Mirror
+                    // Stony Wilderness, Dusty Bowl, Hammerlocke Hills
+                    return new int[] { 144, 146, 148 };
+
+                case 150: // Hammerlocke Hills
+                    // Dusty Bowl, Giant's Mirror, Giant's Cap
+                    return new int[] { 146, 148, 152 };
+
+                case 152: // Giant's Cap
+                    // Stony Wilderness, Giant's Cap
+                    // Also connects to Lake of Outrage but too much of a stretch
+                    return new int[] { 144, 150 };
+
+                case 154: // Lake of Outrage
+                    return new int[] { }; // Empty
+
+                default:
+                    return new int[] { }; // Empty
+            }
+        }
     }
 }
