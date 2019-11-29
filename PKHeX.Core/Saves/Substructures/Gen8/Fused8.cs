48a87397ec887a1d18921bf83c13aee55b54fb67
Kurt <kaphotics@gmail.com>
2019-11-28 21:13:53 -0800

    Split daycare8 to its own file
    Add method to fetch daycare seeds from metadata

--- PKHeX.Core/Saves/Substructures/Gen8/Fused8.cs
+++ PKHeX.Core/Saves/Substructures/Gen8/Fused8.cs
@@ -1,7 +1,8 @@
-﻿using System;
-
-namespace PKHeX.Core
+﻿namespace PKHeX.Core
 {
+    /// <summary>
+    /// Storage for the Fused species, <see cref="Species.Kyurem"/>, <see cref="Species.Solgaleo"/>, <see cref="Species.Lunala"/>
+    /// </summary>
     public sealed class Fused8 : SaveBlock
     {
         public Fused8(SaveFile sav, SCBlock block) : base(sav, block.Data) { }
@@ -11,56 +12,6 @@ namespace PKHeX.Core
             if ((uint)slot >= 3)
                 return -1;
             return PKX.SIZE_8PARTY * slot;
-        }
-    }
-
-    public sealed class Daycare8 : SaveBlock
-    {
-        public Daycare8(SaveFile sav, SCBlock block) : base(sav, block.Data) { }
-
-        private const int STRUCT_SIZE = 4 + PKX.SIZE_8STORED;
-        private const int DAYCARE_SIZE = (2 * STRUCT_SIZE) + 0x26;
-
-        public bool GetDaycare1SlotOccupied(int slot)
-        {
-            if ((uint) slot >= 2)
-                throw new IndexOutOfRangeException(nameof(slot));
-
-            return Data[GetDaycare1StructOffset(slot)] == 1;
-        }
-
-        public bool GetDaycare2SlotOccupied(int slot)
-        {
-            if ((uint)slot >= 2)
-                throw new IndexOutOfRangeException(nameof(slot));
-
-            return Data[GetDaycare2StructOffset(slot)] == 1;
-        }
-
-        public static int GetDaycare1StructOffset(int slot)
-        {
-            if ((uint)slot >= 2)
-                throw new IndexOutOfRangeException(nameof(slot));
-
-            return 0 + (slot * STRUCT_SIZE);
-        }
-
-        public static int GetDaycare2StructOffset(int slot)
-        {
-            if ((uint)slot >= 2)
-                throw new IndexOutOfRangeException(nameof(slot));
-
-            return DAYCARE_SIZE + (slot * STRUCT_SIZE);
-        }
-
-        public static int GetDaycareSlotOffset(int daycare, int slot)
-        {
-            return daycare switch
-            {
-                0 => (4 + GetDaycare1StructOffset(slot)),
-                1 => (4 + GetDaycare2StructOffset(slot)),
-                _ => throw new IndexOutOfRangeException(nameof(daycare))
-            };
         }
     }
 }
