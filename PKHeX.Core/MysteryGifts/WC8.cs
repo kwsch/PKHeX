bc3e84d30ee2768eb7cf91fb85e8bcd134971c7b
Kurt <kaphotics@gmail.com>
2019-12-23 11:27:36 -0800

    Check high bound of language when generating wb7/8
    Closes #2599 ty @slp32

--- PKHeX.Core/MysteryGifts/WC8.cs
+++ PKHeX.Core/MysteryGifts/WC8.cs
@@ -240,7 +240,7 @@ namespace PKHeX.Core
         private static int GetLanguageIndex(int language)
         {
             var lang = (LanguageID) language;
-            if (lang < LanguageID.Japanese || lang == LanguageID.UNUSED_6)
+            if (lang < LanguageID.Japanese || lang == LanguageID.UNUSED_6 || lang > LanguageID.ChineseT)
                 return (int) LanguageID.English; // fallback
             return lang < LanguageID.UNUSED_6 ? language - 1 : language - 2;
         }
