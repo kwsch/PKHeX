429a3b1a08fc4948d96cbbee70536f53b57a79e6
Kurt <kaphotics@gmail.com>
2020-03-17 12:14:01 -0700

    Expose decoration list for editing
    Closes #2785 , not making a GUI for this (someone else can do it)
    
    Moves RTC3 to the appropriate folder

---
+++ PKHeX.Core/Saves/Substructures/Gen3/DecorationInventory3.cs
@@ -0,0 +1,27 @@
+﻿using System.Runtime.InteropServices;
+
+namespace PKHeX.Core
+{
+    [StructLayout(LayoutKind.Sequential)]
+    public struct DecorationInventory3
+    {
+        public const int SIZE = 150;
+
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 10)]
+        public readonly Decoration3[] Desk;
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 10)]
+        public readonly Decoration3[] Chair;
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 10)]
+        public readonly Decoration3[] Plant;
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 30)]
+        public readonly Decoration3[] Ornament;
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 30)]
+        public readonly Decoration3[] Mat;
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 10)]
+        public readonly Decoration3[] Poster;
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 40)]
+        public readonly Decoration3[] Doll;
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 10)]
+        public readonly Decoration3[] Cushion;
+    }
+}
---
+++ PKHeX.Core/Saves/Substructures/Gen3/StructConverter.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace PKHeX.Core
+{
+    internal static class StructConverter
+    {
+        public static T ToStructure<T>(this byte[] bytes) where T : struct
+        {
+            var handle = GCHandle.Alloc(bytes, GCHandleType.Pinned);
+            try { return (T)Marshal.PtrToStructure(handle.AddrOfPinnedObject(), typeof(T)); }
+            finally { handle.Free(); }
+        }
+
+        public static T ToClass<T>(this byte[] bytes) where T : class
+        {
+            var handle = GCHandle.Alloc(bytes, GCHandleType.Pinned);
+            try { return (T)Marshal.PtrToStructure(handle.AddrOfPinnedObject(), typeof(T)); }
+            finally { handle.Free(); }
+        }
+
+        public static byte[] ToBytesClass<T>(this T obj) where T : class
+        {
+            int size = Marshal.SizeOf(obj);
+            byte[] arr = new byte[size];
+
+            IntPtr ptr = Marshal.AllocHGlobal(size);
+            Marshal.StructureToPtr(obj, ptr, true);
+            Marshal.Copy(ptr, arr, 0, size);
+            Marshal.FreeHGlobal(ptr);
+            return arr;
+        }
+
+        public static byte[] ToBytes<T>(this T obj) where T : struct
+        {
+            int size = Marshal.SizeOf(obj);
+            byte[] arr = new byte[size];
+
+            IntPtr ptr = Marshal.AllocHGlobal(size);
+            Marshal.StructureToPtr(obj, ptr, true);
+            Marshal.Copy(ptr, arr, 0, size);
+            Marshal.FreeHGlobal(ptr);
+            return arr;
+        }
+    }
+}
