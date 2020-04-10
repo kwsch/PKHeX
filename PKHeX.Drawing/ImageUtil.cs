775e8a61d4fec406fabd12151f5b803e771d7513
Kurt <kaphotics@gmail.com>
2020-04-09 23:03:12 -0700

    Remove unused method

--- PKHeX.Drawing/ImageUtil.cs
+++ PKHeX.Drawing/ImageUtil.cs
@@ -238,28 +238,5 @@ namespace PKHeX.Drawing
             byte b = (byte)((color.B * amount) + (backColor.B * (1 - amount)));
             return Color.FromArgb(r, g, b);
         }
-
-        // https://stackoverflow.com/a/24199315
-        public static Bitmap ResizeImage(Image image, int width, int height)
-        {
-            var destRect = new Rectangle(0, 0, width, height);
-            var destImage = new Bitmap(width, height);
-
-            destImage.SetResolution(image.HorizontalResolution, image.VerticalResolution);
-
-            using var wrapMode = new ImageAttributes();
-            wrapMode.SetWrapMode(WrapMode.TileFlipXY);
-
-            using var graphics = Graphics.FromImage(destImage);
-            graphics.CompositingMode = CompositingMode.SourceCopy;
-            graphics.CompositingQuality = CompositingQuality.HighQuality;
-            graphics.InterpolationMode = InterpolationMode.HighQualityBicubic;
-            graphics.SmoothingMode = SmoothingMode.HighQuality;
-            graphics.PixelOffsetMode = PixelOffsetMode.HighQuality;
-
-            graphics.DrawImage(image, destRect, 0, 0, image.Width, image.Height, GraphicsUnit.Pixel, wrapMode);
-
-            return destImage;
-        }
     }
 }
