67e9809ecd3132a8a43718127f345e1a6e48d7b5
Kurt <kaphotics@gmail.com>
2020-04-12 19:41:03 -0700

    Require exact form matching

--- PKHeX.Core/Legality/Areas/EncounterArea7b.cs
+++ PKHeX.Core/Legality/Areas/EncounterArea7b.cs
@@ -13,7 +13,7 @@ namespace PKHeX.Core
 
         protected override IEnumerable<EncounterSlot> GetMatchFromEvoLevel(PKM pkm, IEnumerable<EvoCriteria> vs, int minLevel)
         {
-            var slots = Slots.Where(slot => vs.Any(evo => evo.Species == slot.Species && evo.Level >= (slot.LevelMin - CatchComboBonus)));
+            var slots = Slots.Where(slot => vs.Any(evo => evo.Species == slot.Species && evo.Form == slot.Form && evo.Level >= (slot.LevelMin - CatchComboBonus)));
 
             // Get slots where pokemon can exist with respect to level constraints
             return slots.Where(s => s.IsLevelWithinRange(minLevel, minLevel, 0, CatchComboBonus));
@@ -21,28 +21,7 @@ namespace PKHeX.Core
 
         protected override IEnumerable<EncounterSlot> GetFilteredSlots(PKM pkm, IEnumerable<EncounterSlot> slots, int minLevel)
         {
-            int species = pkm.Species;
-
-            int form = pkm.AltForm;
-            if (Legal.AlolanVariantEvolutions12.Contains(species) || Legal.GalarVariantFormEvolutions.Contains(species)) // match form if same species, else form 0.
-            {
-                if (pkm.AltForm != 0 && pkm is PB7)
-                    yield break; // can't get Alolan forms from wild
-
-                foreach (var slot in slots)
-                {
-                    if (species == slot.Species ? slot.Form == form : slot.Form == 0)
-                        yield return slot;
-                }
-            }
-            else if (form == 0)
-            {
-                // enforce no form
-                foreach (var slot in slots)
-                {
-                    yield return slot;
-                }
-            }
+            return slots;
         }
     }
 }
