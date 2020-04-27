8c723b32eef5d9d46070896b22f32b190e522778
Kurt <kaphotics@gmail.com>
2020-04-26 17:53:50 -0700

    Don't show/modify earned bp on RS saves
    Closes #2819

--- PKHeX.WinForms/Subforms/Save Editors/Gen3/SAV_Misc3.cs
+++ PKHeX.WinForms/Subforms/Save Editors/Gen3/SAV_Misc3.cs
@@ -63,10 +63,6 @@ namespace PKHeX.WinForms
             else
             { TB_OTName.Visible = L_TrainerName.Visible = GB_TCM.Visible = false; }
 
-            if (SAV.RS)
-                NUD_BP.Visible = L_BP.Visible = false;
-            else
-                NUD_BP.Value = Math.Min(NUD_BP.Maximum, SAV.BP);
             NUD_Coins.Value = Math.Min(NUD_Coins.Maximum, SAV.Coin);
         }
 
@@ -477,6 +473,19 @@ namespace PKHeX.WinForms
                 if (index == 1)
                     LoadFame(val);
             };
+
+            if (!SAV.RS)
+            {
+                NUD_BP.Value = Math.Min(NUD_BP.Maximum, SAV.BP);
+                NUD_BPEarned.Value = SAV.BPEarned;
+                NUD_BPEarned.ValueChanged += (s, e) => SAV.BPEarned = (uint)NUD_BPEarned.Value;
+            }
+            else
+            {
+                NUD_BP.Visible = L_BP.Visible = false;
+                NUD_BPEarned.Visible = L_BPEarned.Visible = false;
+            }
+
             NUD_FameH.ValueChanged += (s, e) => ChangeFame();
             NUD_FameM.ValueChanged += (s, e) => ChangeFame();
             NUD_FameS.ValueChanged += (s, e) => ChangeFame();
@@ -484,9 +493,6 @@ namespace PKHeX.WinForms
             void ChangeFame() => records.SetRecord(1, (uint)(NUD_RecordValue.Value = GetFameTime()));
             void LoadRecordID(int index) => NUD_RecordValue.Value = records.GetRecord(index);
             void LoadFame(uint val) => SetFameTime(val);
-
-            NUD_BPEarned.Value = SAV.BPEarned;
-            NUD_BPEarned.ValueChanged += (s, e) => SAV.BPEarned = (uint)NUD_BPEarned.Value;
         }
 
         public uint GetFameTime()
