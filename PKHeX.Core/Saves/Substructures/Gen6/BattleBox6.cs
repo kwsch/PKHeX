8ce4f64239aa8c5d703fbd1c9af5a0a0f409f40c
Kurt <kaphotics@gmail.com>
2020-05-24 17:11:39 -0700

    Move files to their correct location

---
+++ PKHeX.Core/Saves/Substructures/Gen6/BattleBox6.cs
@@ -0,0 +1,27 @@
+﻿namespace PKHeX.Core
+{
+    public sealed class BattleBox6 : SaveBlock
+    {
+        public BattleBox6(SaveFile SAV, int offset) : base(SAV) => Offset = offset;
+
+        private int LockedFlagOffset => Offset + (6 * PokeCrypto.SIZE_6STORED);
+
+        public bool Locked
+        {
+            get => LockedWiFiTournament || LockedLiveTournament;
+            set => LockedWiFiTournament = LockedLiveTournament = value;
+        }
+
+        public bool LockedWiFiTournament
+        {
+            get => (Data[LockedFlagOffset] & 1) != 0;
+            set => Data[LockedFlagOffset] = (byte)((Data[Offset + LockedFlagOffset] & ~1) | (value ? 1 : 0));
+        }
+
+        public bool LockedLiveTournament
+        {
+            get => (Data[LockedFlagOffset] & 2) != 0;
+            set => Data[LockedFlagOffset] = (byte)((Data[Offset + LockedFlagOffset] & ~2) | (value ? 2 : 0));
+        }
+    }
+}
--- PKHeX.Core/Saves/Substructures/Gen6/Puff6.cs
+++ PKHeX.Core/Saves/Substructures/Gen6/Puff6.cs
@@ -57,29 +57,4 @@ namespace PKHeX.Core
                 Array.Reverse(Data, Offset, PuffCount);
         }
     }
-
-    public sealed class BattleBox6 : SaveBlock
-    {
-        public BattleBox6(SaveFile SAV, int offset) : base(SAV) => Offset = offset;
-
-        private int LockedFlagOffset => Offset + (6 * PokeCrypto.SIZE_6STORED);
-
-        public bool Locked
-        {
-            get => LockedWiFiTournament || LockedLiveTournament;
-            set => LockedWiFiTournament = LockedLiveTournament = value;
-        }
-
-        public bool LockedWiFiTournament
-        {
-            get => (Data[LockedFlagOffset] & 1) != 0;
-            set => Data[LockedFlagOffset] = (byte)((Data[Offset + LockedFlagOffset] & ~1) | (value ? 1 : 0));
-        }
-
-        public bool LockedLiveTournament
-        {
-            get => (Data[LockedFlagOffset] & 2) != 0;
-            set => Data[LockedFlagOffset] = (byte)((Data[Offset + LockedFlagOffset] & ~2) | (value ? 2 : 0));
-        }
-    }
 }
