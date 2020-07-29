490925641471662c42449f01d585a1c77f812899
Kurt <kaphotics@gmail.com>
2020-07-29 13:36:33 -0700

    Split shadow info entry to separate files
    #2941
    add IV fields

---
+++ PKHeX.Core/Saves/Substructures/Gen3/CXD/ShadowInfoEntryColo.cs
@@ -0,0 +1,15 @@
+﻿namespace PKHeX.Core
+{
+    public sealed class ShadowInfoEntryColo
+    {
+        public readonly byte[] Data;
+        private const int SIZE_ENTRY = 12;
+
+        public ShadowInfoEntryColo() => Data = new byte[SIZE_ENTRY];
+        public ShadowInfoEntryColo(byte[] data) => Data = data;
+
+        public uint PID { get => BigEndian.ToUInt32(Data, 0x00); set => BigEndian.GetBytes(value).CopyTo(Data, 0x00); }
+        public int Met_Location { get => BigEndian.ToUInt16(Data, 0x06); set => BigEndian.GetBytes((ushort)value).CopyTo(Data, 0x06); }
+        public uint Unk08 { get => BigEndian.ToUInt32(Data, 0x08); set => BigEndian.GetBytes(value).CopyTo(Data, 0x08); }
+    }
+}
