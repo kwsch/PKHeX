816f35d042d6163975ad2c39ccc3e196e1514b33
Kurt <kaphotics@gmail.com>
2020-08-22 11:59:43 -0700

    Split file

--- PKHeX.Core/Legality/Encounters/EncounterStatic/EncounterStatic.cs
+++ PKHeX.Core/Legality/Encounters/EncounterStatic/EncounterStatic.cs
@@ -322,15 +322,4 @@ namespace PKHeX.Core
             return false;
         }
     }
-
-    public sealed class EncounterStatic6 : EncounterStatic, IContestStats
-    {
-        internal IReadOnlyList<int> Contest { set => this.SetContestStats(value); }
-        public int CNT_Cool { get; set; }
-        public int CNT_Beauty { get; set; }
-        public int CNT_Cute { get; set; }
-        public int CNT_Smart { get; set; }
-        public int CNT_Tough { get; set; }
-        public int CNT_Sheen { get; set; }
-    }
 }
---
+++ PKHeX.Core/Legality/Encounters/EncounterStatic/EncounterStatic6.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace PKHeX.Core
+{
+    public sealed class EncounterStatic6 : EncounterStatic, IContestStats
+    {
+        internal IReadOnlyList<int> Contest { set => this.SetContestStats(value); }
+        public int CNT_Cool { get; set; }
+        public int CNT_Beauty { get; set; }
+        public int CNT_Cute { get; set; }
+        public int CNT_Smart { get; set; }
+        public int CNT_Tough { get; set; }
+        public int CNT_Sheen { get; set; }
+    }
+}
