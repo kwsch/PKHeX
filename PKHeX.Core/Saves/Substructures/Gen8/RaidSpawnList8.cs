919f3f28933edb7998fdc493aa4d7acdd37c63a9
Kurt <kaphotics@gmail.com>
2020-10-24 12:38:33 -0700

    Update RaidSpawnList8.cs
    Co-Authored-By: Leanny <1884062+Leanny@users.noreply.github.com>

--- PKHeX.Core/Saves/Substructures/Gen8/RaidSpawnList8.cs
+++ PKHeX.Core/Saves/Substructures/Gen8/RaidSpawnList8.cs
@@ -16,7 +16,7 @@ namespace PKHeX.Core
 
         public const int RaidCountLegal_O0 = 100;
         public const int RaidCountLegal_R1 = 90;
-        public const int RaidCountLegal_R2 = 90;
+        public const int RaidCountLegal_R2 = 86;
 
         public RaidSpawnDetail GetRaid(int entry) => new RaidSpawnDetail(Data, entry * RaidSpawnDetail.SIZE);
 
