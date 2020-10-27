5b313210edab0c13f0a6ea9bcd567b644b6f8e98
Kurt <kaphotics@gmail.com>
2020-10-27 09:24:06 -0700

    Handle SWSH gender strings same as LGPE
    See https://github.com/kwsch/PKHeX/commit/6a0a6aa82aea7b61a6ceac9e8614abdbf537f69c for more context

--- PKHeX.Core/Saves/SAV8.cs
+++ PKHeX.Core/Saves/SAV8.cs
@@ -78,7 +78,7 @@ namespace PKHeX.Core
         {
             if (PadToSize == 0)
                 PadToSize = maxLength + 1;
-            return StringConverter.SetString7(value, maxLength, Language, PadToSize, PadWith);
+            return StringConverter.SetString7b(value, maxLength, Language, PadToSize, PadWith);
         }
 
         // Player Information
