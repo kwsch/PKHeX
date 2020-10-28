cb545781b0daf049770cfe21aec14aec262416f8
Kurt <kaphotics@gmail.com>
2020-10-27 18:03:45 -0700

    Split off es5dr to separate file, require never shiny
    Shiny check was mistakenly forgotten :)

--- PKHeX.Core/Legality/Encounters/EncounterStatic/EncounterStatic5.cs
+++ PKHeX.Core/Legality/Encounters/EncounterStatic/EncounterStatic5.cs
@@ -32,30 +32,4 @@ namespace PKHeX.Core
             22,23,24,    // Route 9, 10, 11 Night former half
         };
     }
-
-    /// <summary>
-    /// Generation 5 Dream Radar gift encounters
-    /// </summary>
-    public sealed class EncounterStatic5DR : EncounterStatic5
-    {
-        public EncounterStatic5DR(int species, int form, int abilityIndex = 4)
-        {
-            Species = species;
-            Form = form;
-            Ability = abilityIndex;
-            Location = 30015;
-            Gift = true;
-            Ball = 25;
-            Level = 5; // to 40
-        }
-
-        protected override bool IsMatchLevel(PKM pkm, DexLevel evo)
-        {
-            // Level from 5->40 depends on the number of badges
-            var met = pkm.Met_Level;
-            if (met % 5 != 0)
-                return false;
-            return (uint) (met - 5) <= 35; // 5 <= x <= 40
-        }
-    }
 }
