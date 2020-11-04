62073f5fd9e315352ae4c9ed609647ff0312fa76
Kurt <kaphotics@gmail.com>
2020-11-03 19:28:41 -0800

    Update GP1.cs

--- PKHeX.Core/Saves/Substructures/Gen7/GP1.cs
+++ PKHeX.Core/Saves/Substructures/Gen7/GP1.cs
@@ -176,8 +176,10 @@ namespace PKHeX.Core
             pk.Nature = (int)criteria.GetNature(Nature.Random);
             pk.RefreshAbility(criteria.GetAbilityFromType(av, pi));
 
-            if (IsShiny)
-                pk.SetShiny();
+            if (IsShiny) // Force Square
+                pk.PID = (uint)(((sav.TID ^ sav.SID ^ (pk.PID & 0xFFFF) ^ 0) << 16) | (pk.PID & 0xFFFF));
+            else if (pk.IsShiny)
+                pk.PID ^= 0x1000_0000;
 
             var moves = MoveLevelUp.GetEncounterMoves(pk, Level, GameVersion.GO);
             pk.Moves = moves;
