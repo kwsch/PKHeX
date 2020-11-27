3680048c0a2dc7f74772e3a8720acdeb74c993e1
Kurt <kaphotics@gmail.com>
2020-11-27 12:14:13 -0800

    Add local pickle loading if compiled as debug
    Same dangers about "what is the root path"
    
    Bump the level cap while we're here.

--- PKHeX.Core/Legality/Encounters/Data/EncountersGO.cs
+++ PKHeX.Core/Legality/Encounters/Data/EncountersGO.cs
@@ -5,11 +5,20 @@
     /// </summary>
     internal static class EncountersGO
     {
-        internal const int MAX_LEVEL = 40;
+        internal const int MAX_LEVEL = 50;
 
         internal static readonly EncounterArea7g[] SlotsGO_GG = EncounterArea7g.GetArea(Get("go_lgpe", "go"));
         internal static readonly EncounterArea8g[] SlotsGO = EncounterArea8g.GetArea(Get("go_home", "go"));
 
-        private static byte[][] Get(string resource, string ident) => BinLinker.Unpack(Util.GetBinaryResource($"encounter_{resource}.pkl"), ident);
+        private static byte[][] Get(string resource, string ident)
+        {
+            var name = $"encounter_{resource}.pkl";
+#if DEBUG
+            var data = System.IO.File.Exists(name) ? System.IO.File.ReadAllBytes(name) : Util.GetBinaryResource(name);
+#else
+            var data = Util.GetBinaryResource(name);
+#endif
+            return BinLinker.Unpack(data, ident);
+        }
     }
 }
