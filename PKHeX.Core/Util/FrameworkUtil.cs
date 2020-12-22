6d030831ab427449c7cfaa224fe13687bff7d105
Kurt <kaphotics@gmail.com>
2020-12-21 16:52:11 -0800

    Bump PKHeX.Core to C#9 language version
    Add FrameworkUtil.cs which is compiled if the build isn't net5+, needed to support init setters & records

---
+++ PKHeX.Core/Util/FrameworkUtil.cs
@@ -0,0 +1,21 @@
+﻿#if !NET5
+#pragma warning disable
+// ReSharper disable once UnusedType.Global
+
+namespace System.Runtime.CompilerServices
+{
+    using Diagnostics;
+    using Diagnostics.CodeAnalysis;
+
+    /// <summary>
+    ///     Reserved to be used by the compiler for tracking metadata.
+    ///     This class should not be used by developers in source code.
+    /// </summary>
+    [ExcludeFromCodeCoverage, DebuggerNonUserCode]
+#endif
+    internal static class IsExternalInit
+    {
+    }
+}
+
+#pragma warning restore
--- PKHeX.Drawing/Properties/Resources.Designer.cs
+++ PKHeX.Drawing/Properties/Resources.Designer.cs
@@ -8,6 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
+[assembly: System.Resources.NeutralResourcesLanguage("en")]
 namespace PKHeX.Drawing.Properties {
     using System;
     
