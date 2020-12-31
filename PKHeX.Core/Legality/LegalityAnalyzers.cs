28ec0e91f08346e1d5516f0af8383db90fa3749b
Kurt <kaphotics@gmail.com>
2020-12-31 10:33:30 -0800

    Use specific type when storing reference

--- PKHeX.Core/Legality/LegalityAnalyzers.cs
+++ PKHeX.Core/Legality/LegalityAnalyzers.cs
@@ -2,26 +2,26 @@
 {
     internal static class LegalityAnalyzers
     {
-        public static readonly Verifier LanguageIndex = new LanguageVerifier();
-        public static readonly Verifier Nickname = new NicknameVerifier();
-        public static readonly Verifier EffortValues = new EffortValueVerifier();
-        public static readonly Verifier IndividualValues = new IndividualValueVerifier();
-        public static readonly Verifier BallIndex = new BallVerifier();
-        public static readonly Verifier FormValues = new FormVerifier();
-        public static readonly Verifier ConsoleRegion = new ConsoleRegionVerifier();
-        public static readonly Verifier AbilityValues = new AbilityVerifier();
-        public static readonly Verifier Medal = new MedalVerifier();
-        public static readonly Verifier Ribbon = new RibbonVerifier();
-        public static readonly Verifier Item = new ItemVerifier();
-        public static readonly Verifier Gen4EncounterType = new EncounterTypeVerifier();
-        public static readonly Verifier HyperTraining = new HyperTrainingVerifier();
-        public static readonly Verifier GenderValues = new GenderVerifier();
-        public static readonly Verifier PIDEC = new PIDVerifier();
-        public static readonly Verifier NHarmonia = new NHarmoniaVerifier();
-        public static readonly Verifier CXD = new CXDVerifier();
-        public static readonly Verifier Memory = new MemoryVerifier();
-        public static readonly Verifier History = new HistoryVerifier();
-        public static readonly Verifier Contest = new ContestStatVerifier();
+        public static readonly LanguageVerifier LanguageIndex = new();
+        public static readonly NicknameVerifier Nickname = new();
+        public static readonly EffortValueVerifier EffortValues = new();
+        public static readonly IndividualValueVerifier IndividualValues = new();
+        public static readonly BallVerifier BallIndex = new();
+        public static readonly FormVerifier FormValues = new();
+        public static readonly ConsoleRegionVerifier ConsoleRegion = new();
+        public static readonly AbilityVerifier AbilityValues = new();
+        public static readonly MedalVerifier Medal = new();
+        public static readonly RibbonVerifier Ribbon = new();
+        public static readonly ItemVerifier Item = new();
+        public static readonly EncounterTypeVerifier Gen4EncounterType = new();
+        public static readonly HyperTrainingVerifier HyperTraining = new();
+        public static readonly GenderVerifier GenderValues = new();
+        public static readonly PIDVerifier PIDEC = new();
+        public static readonly NHarmoniaVerifier NHarmonia = new();
+        public static readonly CXDVerifier CXD = new();
+        public static readonly MemoryVerifier Memory = new();
+        public static readonly HistoryVerifier History = new();
+        public static readonly ContestStatVerifier Contest = new();
 
         public static readonly TrainerNameVerifier Trainer = new();
         public static readonly LevelVerifier Level = new();
