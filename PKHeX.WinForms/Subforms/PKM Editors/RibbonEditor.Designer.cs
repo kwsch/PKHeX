d5e9775081419171894187f8ffadaaa32479c712
Kurt <kaphotics@gmail.com>
2021-02-10 14:12:14 -0800

    Update RibbonEditor.Designer.cs
    https://github.com/kwsch/PKHeX/issues/3146#issuecomment-777060691

--- PKHeX.WinForms/Subforms/PKM Editors/RibbonEditor.Designer.cs
+++ PKHeX.WinForms/Subforms/PKM Editors/RibbonEditor.Designer.cs
@@ -154,7 +154,7 @@
             // 
             // CB_Affixed
             // 
-            this.CB_Affixed.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            this.CB_Affixed.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.CB_Affixed.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.CB_Affixed.DropDownWidth = 200;
