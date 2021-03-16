4d6e77b2bfc0fa5f41fd99f81b1e57728f3dc991
Archit Date <architdate@gmail.com>
2021-03-16 21:48:16 +0800

    fix sos encounters ability assignment

--- PKHeX.Core/Legality/Encounters/EncounterSlot/EncounterSlot7.cs
+++ PKHeX.Core/Legality/Encounters/EncounterSlot/EncounterSlot7.cs
@@ -16,6 +16,21 @@ namespace PKHeX.Core
         {
             base.SetPINGA(pk, criteria);
             pk.SetRandomEC();
+            if (Area.Type is not SlotType.SOS)
+                return;
+
+            var ivs = new int[] { criteria.IV_HP, criteria.IV_ATK, criteria.IV_DEF,
+                                 criteria.IV_SPE, criteria.IV_SPA, criteria.IV_SPD };
+            var flawless = 0;
+            foreach (var iv in ivs)
+                if (iv == 31) flawless++;
+
+            if (flawless >= 2)
+                return;
+
+            var abilities = pk.PersonalInfo.Abilities;
+            if (abilities[0] == criteria.Ability) pk.RefreshAbility(0);
+            if (abilities[1] == criteria.Ability) pk.RefreshAbility(1);
         }
 
         protected override HiddenAbilityPermission IsHiddenAbilitySlot() => Area.Type == SlotType.SOS ? HiddenAbilityPermission.Possible : HiddenAbilityPermission.Never;
