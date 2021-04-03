c90f912fcca577e9af513f882557a1bbfd383a64
Kurt <kaphotics@gmail.com>
2021-04-02 18:41:11 -0700

    Add test to prove failure per Anubis' provided test

--- Tests/PKHeX.Core.Tests/Legality/BreedTests.cs
+++ Tests/PKHeX.Core.Tests/Legality/BreedTests.cs
@@ -7,11 +7,19 @@ namespace PKHeX.Tests.Legality
     public class BreedTests
     {
         [Theory]
-        [InlineData(2, GameVersion.GD, 5, Species.Bulbasaur, 0, (int)Move.Tackle, (int)Move.Growl, 0, 0)]
+        [InlineData(2, GameVersion.GD, 5, Species.Bulbasaur, 0, (int)Move.Tackle, (int)Move.Growl)]
         public void VerifyBreed2(int gen, GameVersion game, int lvl, Species species, int form, params int[] moves)
         {
             var test = MoveBreed.Process25(gen, (int) species, form, game, moves, lvl);
             test.Should().BeTrue();
+        }
+
+        [Theory]
+        [InlineData(8, GameVersion.SH, 1, Species.Honedge, 0, (int)Move.FuryCutter, (int)Move.WideGuard, (int)Move.DestinyBond)]
+        public void CheckBad6(int gen, GameVersion game, int lvl, Species species, int form, params int[] moves)
+        {
+            var test = MoveBreed.Process6(gen, (int)species, form, game, moves, lvl);
+            test.Should().BeFalse();
         }
     }
 }
