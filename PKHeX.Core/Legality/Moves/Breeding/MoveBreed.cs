219bf7e0c81dc57434d8b1e6955c4511934615fc
Kurt <kaphotics@gmail.com>
2021-04-07 19:23:39 -0700

    Update MoveBreed.cs

--- PKHeX.Core/Legality/Moves/Breeding/MoveBreed.cs
+++ PKHeX.Core/Legality/Moves/Breeding/MoveBreed.cs
@@ -84,7 +84,7 @@ namespace PKHeX.Core
                 if (m.Source == 0)
                     continue; // invalid
                 int move = m.Move;
-                if (baseMoves.IndexOf(move) != -1)
+                if (baseMoves.IndexOf(move) == -1)
                     notBase.Add(move);
             }
 
@@ -93,9 +93,9 @@ namespace PKHeX.Core
                 baseCount = baseMoves.Length;
             int ctr = 0;
             for (; ctr < baseCount; ctr++)
-                result[ctr] = baseMoves[baseMoves.Length - baseCount + ctr];
+                result[ctr++] = baseMoves[baseMoves.Length - baseCount + ctr];
             foreach (var m in notBase)
-                result[ctr] = m;
+                result[ctr++] = m;
 
             for (int i = ctr; i < result.Length; i++)
                 result[i] = 0;
