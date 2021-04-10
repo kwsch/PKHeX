9bc07b6e01ea5428d80e752acbf9768a8ac9d5b0
Kurt <kaphotics@gmail.com>
2021-04-09 17:20:30 -0700

    Remove unused setting

--- PKHeX.WinForms/Properties/Settings.Designer.cs
+++ PKHeX.WinForms/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace PKHeX.WinForms.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.4.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.8.1.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -344,18 +344,6 @@ namespace PKHeX.WinForms.Properties {
             }
             set {
                 this["ApplyNature"] = value;
-            }
-        }
-        
-        [global::System.Configuration.UserScopedSettingAttribute()]
-        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("True")]
-        public bool UseLargeSprites {
-            get {
-                return ((bool)(this["UseLargeSprites"]));
-            }
-            set {
-                this["UseLargeSprites"] = value;
             }
         }
     }
