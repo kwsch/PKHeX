8e52d2fddbd0d3ff237ac75943d4d0c3871dfc0e
Kurt <kaphotics@gmail.com>
2021-04-17 13:07:28 -0700

    Xmldoc updates

--- PKHeX.Core/Legality/Moves/Breeding/EggSource.cs
+++ PKHeX.Core/Legality/Moves/Breeding/EggSource.cs
@@ -3,61 +3,97 @@ using static PKHeX.Core.LegalityCheckStrings;
 
 namespace PKHeX.Core
 {
+    /// <summary>
+    /// Egg Moveset Building Order for Generation 2
+    /// </summary>
     public enum EggSource2 : byte
     {
         None,
+        /// <summary> Initial moveset for the egg's level. </summary>
         Base,
+        /// <summary> Egg move inherited from the Father. </summary>
         FatherEgg,
+        /// <summary> Technical Machine move inherited from the Father. </summary>
         FatherTM,
+        /// <summary> Level Up move inherited from a parent. </summary>
         ParentLevelUp,
+        /// <summary> Tutor move (Elemental Beam) inherited from a parent. </summary>
         Tutor,
 
         Max,
     }
 
+    /// <summary>
+    /// Egg Moveset Building Order for Generation 3 &amp; 4
+    /// </summary>
     public enum EggSource34 : byte
     {
         None,
+        /// <summary> Initial moveset for the egg's level. </summary>
         Base,
+        /// <summary> Egg move inherited from the Father. </summary>
         FatherEgg,
+        /// <summary> Technical Machine move inherited from the Father. </summary>
         FatherTM,
+        /// <summary> Level Up move inherited from a parent. </summary>
         ParentLevelUp,
 
         Max,
 
+        /// <summary> Special Move applied at the end if certain conditions are satisfied. </summary>
         VoltTackle,
     }
 
+    /// <summary>
+    /// Egg Moveset Building Order for Generation 5
+    /// </summary>
     public enum EggSource5 : byte
     {
         None,
+        /// <summary> Initial moveset for the egg's level. </summary>
         Base,
+        /// <summary> Egg move inherited from the Father. </summary>
         FatherEgg,
+        /// <summary> Technical Machine move inherited from the Father. </summary>
         ParentLevelUp,
-        FatherTM, // after level up, unlike Gen3/4!
+        /// <summary> Technical Machine move inherited from the Father. </summary>
+        /// <remarks> After level up, unlike Gen3/4! </remarks>
+        FatherTM,
 
         Max,
 
+        /// <summary> Special Move applied at the end if certain conditions are satisfied. </summary>
         VoltTackle,
     }
 
+    /// <summary>
+    /// Egg Moveset Building Order for Generation 6+
+    /// </summary>
     public enum EggSource6 : byte
     {
         None,
+        /// <summary> Initial moveset for the egg's level. </summary>
         Base,
+        /// <summary> Level Up move inherited from a parent. </summary>
         ParentLevelUp,
+        /// <summary> Egg move inherited from a parent. </summary>
         ParentEgg,
 
         Max,
 
+        /// <summary> Special Move applied at the end if certain conditions are satisfied. </summary>
         VoltTackle,
     }
 
+    /// <summary>
+    /// Utility logic for converting a <see cref="MoveBreed"/> move result into a user friendly string.
+    /// </summary>
     public static class EggSourceUtil
     {
-#pragma warning disable RCS1224 // Make method an extension method.
-        public static string GetSource(object parse, int gen, int index)
-#pragma warning restore RCS1224 // Make method an extension method.
+        /// <summary>
+        /// Unboxes the parse result and returns a user friendly string for the move result.
+        /// </summary>
+        public static string GetSource(object parse, int generation, int index)
         {
             static string GetLine<T>(T[] arr, Func<T, string> act, int i)
             {
@@ -66,7 +102,7 @@ namespace PKHeX.Core
                 return act(arr[i]);
             }
 
-            return gen switch
+            return generation switch
             {
                 2      => GetLine((EggSource2[]) parse, GetSource, index),
                 3 or 4 => GetLine((EggSource34[])parse, GetSource, index),
