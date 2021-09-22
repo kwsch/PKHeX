6a2f778c9ad9b7a9ac13d74849f5ecb23da33b63
Kurt <kaphotics@gmail.com>
2021-09-21 19:42:14 -0700

    Allow reload for debug build

--- PKHeX.Core/Legality/Encounters/Data/EncountersGO.cs
+++ PKHeX.Core/Legality/Encounters/Data/EncountersGO.cs
@@ -3,6 +3,7 @@
     /// <summary>
     /// Encounter data from <see cref="GameVersion.GO"/>, which has multiple generations of origin.
     /// </summary>
+#if !DEBUG
     internal static class EncountersGO
     {
         internal const int MAX_LEVEL = 50;
@@ -13,12 +14,30 @@
         private static byte[][] Get(string resource, string ident)
         {
             var name = $"encounter_{resource}.pkl";
-#if DEBUG
-            var data = System.IO.File.Exists(name) ? System.IO.File.ReadAllBytes(name) : Util.GetBinaryResource(name);
-#else
             var data = Util.GetBinaryResource(name);
-#endif
             return BinLinker.Unpack(data, ident);
         }
     }
+#else
+    public static class EncountersGO
+    {
+        internal const int MAX_LEVEL = 50;
+
+        internal static EncounterArea7g[] SlotsGO_GG = EncounterArea7g.GetArea(Get("go_lgpe", "go"));
+        internal static EncounterArea8g[] SlotsGO = EncounterArea8g.GetArea(Get("go_home", "go"));
+
+        public static void Reload()
+        {
+            SlotsGO_GG = EncounterArea7g.GetArea(Get("go_lgpe", "go"));
+            SlotsGO = EncounterArea8g.GetArea(Get("go_home", "go"));
+        }
+
+        private static byte[][] Get(string resource, string ident)
+        {
+            var name = $"encounter_{resource}.pkl";
+            var data = System.IO.File.Exists(name) ? System.IO.File.ReadAllBytes(name) : Util.GetBinaryResource(name);
+            return BinLinker.Unpack(data, ident);
+        }
+    }
+#endif
 }
