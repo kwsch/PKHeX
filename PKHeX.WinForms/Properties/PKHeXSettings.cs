30b00a4b5aad1fc146b50c4924d217541a3987e6
Kurt <kaphotics@gmail.com>
2021-10-22 20:57:56 -0700

    Add metadata flagging for values
    Allows users to set a minimum importance for the list displayed.
    
    Could probably make this checkboxes within the flags UI, but meh

--- PKHeX.WinForms/Properties/PKHeXSettings.cs
+++ PKHeX.WinForms/Properties/PKHeXSettings.cs
@@ -228,6 +228,9 @@ namespace PKHeX.WinForms
 
         [LocalizedDescription("Path to a dump of block hash-names. If file does not exist, only names defined within the program's code will be loaded.")]
         public string PathBlockKeyListSWSH { get; set; } = "SCBlocks.txt";
+
+        [LocalizedDescription("Hide event variables below this event type value. Removes event values from the GUI that the user doesn't care to view.")]
+        public NamedEventType HideEventTypeBelow { get; set; }
     }
 
     [Serializable]
