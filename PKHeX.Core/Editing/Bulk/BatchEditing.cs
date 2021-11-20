b25a7b621f0ff2918c9e33ec4cb7c6d6d7aab4f4
Kurt <kaphotics@gmail.com>
2021-11-19 22:38:37 -0800

    Update BatchEditing.cs

--- PKHeX.Core/Editing/Bulk/BatchEditing.cs
+++ PKHeX.Core/Editing/Bulk/BatchEditing.cs
@@ -18,7 +18,7 @@ namespace PKHeX.Core
     {
         public static readonly Type[] Types =
         {
-            typeof (PK8),
+            typeof (PK8), typeof (PB8),
             typeof (PB7),
             typeof (PK7), typeof (PK6), typeof (PK5), typeof (PK4), typeof(BK4),
             typeof (PK3), typeof (XK3), typeof (CK3),
