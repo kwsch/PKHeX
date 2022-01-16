93083616ddae1a50b847df3f8e34ab3bb07718d3
sora10pls <17801814+sora10pls@users.noreply.github.com>
2022-01-16 14:53:04 -0500

    Extend Ranked Ribbon availability for GO encounters
    Also hange >= to > because of new GO => HOME met date legality.9/1/2022 is legal if it was transferred in somewhere like Kiribati or New Zealand, but 9/2/2022 isn't.

--- PKHeX.Core/Legality/Verifiers/Ribbons/RibbonVerifier.cs
+++ PKHeX.Core/Legality/Verifiers/Ribbons/RibbonVerifier.cs
@@ -492,7 +492,7 @@ namespace PKHeX.Core
 
                 if (enc.Version == GameVersion.GO) // Capture date is global time, and not console changeable.
                 {
-                    if (pkm.MetDate >= new DateTime(2022, 2, 1))
+                    if (pkm.MetDate > new DateTime(2022, 9, 1)) // Series 12 end date
                         return false;
                 }
             }
