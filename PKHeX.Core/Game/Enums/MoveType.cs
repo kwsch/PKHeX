b38f2dc8c602389a75c3691e76e6d8926602f7cf
Kurt <kaphotics@gmail.com>
2022-07-19 17:51:13 -0700

    Number move types
    Easier reference

--- PKHeX.Core/Game/Enums/MoveType.cs
+++ PKHeX.Core/Game/Enums/MoveType.cs
@@ -1,4 +1,4 @@
-﻿namespace PKHeX.Core;
+namespace PKHeX.Core;
 
 /// <summary>
 /// Elemental type a move has; additionally, types a <see cref="PKM"/> can have.
@@ -6,24 +6,24 @@
 public enum MoveType : sbyte
 {
     Any = -1,
-    Normal,
-    Fighting,
-    Flying,
-    Poison,
-    Ground,
-    Rock,
-    Bug,
-    Ghost,
-    Steel,
-    Fire,
-    Water,
-    Grass,
-    Electric,
-    Psychic,
-    Ice,
-    Dragon,
-    Dark,
-    Fairy,
+    Normal = 0,
+    Fighting = 1,
+    Flying = 2,
+    Poison = 3,
+    Ground = 4,
+    Rock = 5,
+    Bug = 6,
+    Ghost = 7,
+    Steel = 8,
+    Fire = 9,
+    Water = 10,
+    Grass = 11,
+    Electric = 12,
+    Psychic = 13,
+    Ice = 14,
+    Dragon = 15,
+    Dark = 16,
+    Fairy = 17,
 }
 
 public static class MoveTypeExtensions
