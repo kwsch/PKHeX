d3378f84d78a28ec6293ea71f36b697ca5fbc225
Zazsona <40435178+Zazsona@users.noreply.github.com>
2022-09-15 23:51:25 +0100

    Add RanchToy

---
+++ PKHeX.Core/Saves/Substructures/Gen4/RanchToy.cs
@@ -0,0 +1,34 @@
+namespace PKHeX.Core;
+
+/// <summary>
+/// Toys used in My Pokemon Ranch save files.
+/// </summary>
+public enum RanchToy
+{
+    None,
+    Balloons,
+    SlipperyPeel,
+    Bell,
+    BounceBackBall,
+    Rocket,
+    Cushion,
+    ParadeDrum,
+    Bonfire,
+    LeaderFlag,
+    Fountain,
+    IceBlock,
+    Microphone,
+    BurstBall,
+    Palette,
+    Pendulum,
+    Pitfall,
+    TrainingBag,
+    StinkyBall,
+    Snowman,
+    RoundRock,
+    SpinRide,
+    SprintStand,
+    Attractor,
+    Challenger,
+    ToyBox
+}
