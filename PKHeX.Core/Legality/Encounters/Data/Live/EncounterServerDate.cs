86ea3be102379d3b513ec66bd74b147c822e7e31
Manu <52102823+Manu098vm@users.noreply.github.com>
2023-02-16 16:00:08 +0100

    fix flabébé wc9 details

--- PKHeX.Core/Legality/Encounters/Data/Live/EncounterServerDate.cs
+++ PKHeX.Core/Legality/Encounters/Data/Live/EncounterServerDate.cs
@@ -105,6 +105,6 @@ public static class EncounterServerDate
     /// </summary>
     public static readonly Dictionary<int, (DateOnly Start, DateOnly? End)> WC9Gifts = new()
     {
-        {1501,  (new(2022, 11, 17), Never)}, // Flabébé Pokécenter
+        {0001,  (new(2022, 11, 17), Never)}, // PokéCenter Flabébé
     };
 }
