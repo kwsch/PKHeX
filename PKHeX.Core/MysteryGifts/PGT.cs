340578b67152ff84b1cf8c83a2c413c76791a108
Kurt <kaphotics@gmail.com>
2023-04-04 15:03:14 -0700

    Update PGT.cs

--- PKHeX.Core/MysteryGifts/PGT.cs
+++ PKHeX.Core/MysteryGifts/PGT.cs
@@ -12,7 +12,7 @@ public sealed class PGT : DataMysteryGift, IRibbonSetEvent3, IRibbonSetEvent4
     public override int Generation => 4;
     public override EntityContext Context => EntityContext.Gen4;
     public override bool FatefulEncounter => IsManaphyEgg || PK.FatefulEncounter;
-    public override GameVersion Version { get => (GameVersion)PK.Version; set => PK.Version = (int)value; }
+    public override GameVersion Version { get => IsManaphyEgg ? GameVersion.Gen4 : (GameVersion)PK.Version; set => PK.Version = (int)value; }
 
     public override byte Level
     {
