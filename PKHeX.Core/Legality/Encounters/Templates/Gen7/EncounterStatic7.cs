799a9a5af9d9cf440ccf40defc37cfddf991f6f1
Kurt <kaphotics@gmail.com>
2023-08-19 15:42:17 -0700

    Add other missing fateful setters

--- PKHeX.Core/Legality/Encounters/Templates/Gen7/EncounterStatic7.cs
+++ PKHeX.Core/Legality/Encounters/Templates/Gen7/EncounterStatic7.cs
@@ -65,6 +65,7 @@ public sealed record EncounterStatic7(GameVersion Version)
             Met_Level = LevelMin,
             MetDate = EncounterDate.GetDate3DS(),
             Ball = (byte)(FixedBall is Ball.None ? Ball.Poke : FixedBall),
+            FatefulEncounter = FatefulEncounter,
 
             ID32 = tr.ID32,
             Version = (byte)version,
