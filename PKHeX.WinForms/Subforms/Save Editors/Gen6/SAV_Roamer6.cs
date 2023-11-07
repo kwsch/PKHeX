1fdee1880cf3e6d9384665f45f0083e76d256ef0
Jonathan Herbert <foohyfooh@gmail.com>
2023-11-06 22:57:21 -0400

    Add GUI For XY Roamer
    - Add GUI For XY Roamer
    - Change Encout6 to use Memory since Slice made a copy which meant that changing values on Roamer6 didn't persist back to the underlying data without reassigning Encount.

---
+++ PKHeX.WinForms/Subforms/Save Editors/Gen6/SAV_Roamer6.cs
@@ -0,0 +1,41 @@
+using PKHeX.Core;
+using System;
+using System.Windows.Forms;
+
+namespace PKHeX.WinForms;
+
+public partial class SAV_Roamer6 : Form
+{
+    private const int SpeciesOffset = 144;
+    private readonly SAV6XY Origin;
+    private readonly SAV6XY SAV;
+    private readonly Roamer6 roamer;
+
+    public SAV_Roamer6(SAV6XY sav)
+    {
+        InitializeComponent();
+        WinFormsUtil.TranslateInterface(this, Main.CurrentLanguage);
+        SAV = (SAV6XY)(Origin = sav).Clone();
+        roamer = SAV.Encount.Roamer;
+
+        CB_Species.Items.AddRange(new[] { "Articuno", "Zapdos", "Moltres" });
+        CB_Species.SelectedIndex = roamer.Species - SpeciesOffset;
+        NUD_TimesEncountered.Value = roamer.TimesEncountered;
+        CB_RoamState.Items.AddRange(new[] { "Inactive", "Roaming", "Stationary", "Defeated", "Captured" });
+        CB_RoamState.SelectedIndex = (int)roamer.RoamStatus;
+    }
+
+    private void CB_Species_SelectedIndexChanged(object sender, EventArgs e) => roamer.Species = (ushort)(SpeciesOffset + CB_Species.SelectedIndex);
+
+    private void NUD_TimesEncountered_ValueChanged(object sender, EventArgs e) => roamer.TimesEncountered = (uint)NUD_TimesEncountered.Value;
+
+    private void CB_RoamState_SelectedIndexChanged(object sender, EventArgs e) => roamer.RoamStatus = (Roamer6State)CB_RoamState.SelectedIndex;
+
+    private void B_Cancel_Click(object sender, EventArgs e) => Close();
+
+    private void B_Save_Click(object sender, EventArgs e)
+    {
+        Origin.CopyChangesFrom(SAV);
+        Close();
+    }
+}
