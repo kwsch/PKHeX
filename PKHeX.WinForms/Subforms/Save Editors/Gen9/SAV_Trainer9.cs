bd4b1ba3e4625ba4374c5bad5def0309d484570e
Jonathan Herbert <foohyfooh@gmail.com>
2024-01-21 14:20:25 -0400

    Gen 5 Misc and Gen 9 Trainer UI Edits
    Gen 5 Misc Edits
    - Fix duplicate unlock all musical props button caused by me
    
    Gen 9 Trainer Editor
    - Move Gender next to Trainer Name
    - Group Blueberry Quests on Blueberry Tab
    - Move Indigo Disk related unlock buttons to Blueberry Tab
    - Disable Unlock All Buttons after operations

--- PKHeX.WinForms/Subforms/Save Editors/Gen9/SAV_Trainer9.cs
+++ PKHeX.WinForms/Subforms/Save Editors/Gen9/SAV_Trainer9.cs
@@ -49,10 +49,6 @@ public partial class SAV_Trainer9 : Form
         else
             TC_Editor.TabPages.Remove(Tab_Blueberry);
 
-        B_ActivateSnacksworthLegendaries.Visible = SAV.SaveRevision >= 2;
-        B_UnlockCoaches.Visible = SAV.SaveRevision >= 2;
-        B_UnlockThrowStyles.Visible = SAV.SaveRevision >= 2;
-
         Loading = false;
     }
 
@@ -212,9 +208,7 @@ public partial class SAV_Trainer9 : Form
         if (Util.ToInt32(box.Text) > 255) box.Text = "255";
     }
 
-    private void B_UnlockFlyLocations_Click(object sender, EventArgs e) => UnlockFlyLocations();
-
-    private void UnlockFlyLocations()
+    private void B_UnlockFlyLocations_Click(object sender, EventArgs e)
     {
         var accessor = SAV.Accessor;
         foreach (var hash in FlyHashes)
@@ -222,6 +216,7 @@ public partial class SAV_Trainer9 : Form
             if (accessor.TryGetBlock(hash, out var block))
                 block.ChangeBooleanType(SCTypeCode.Bool2);
         }
+        B_UnlockFlyLocations.Enabled = false;
         System.Media.SystemSounds.Asterisk.Play();
     }
 
@@ -346,24 +341,28 @@ public partial class SAV_Trainer9 : Form
     private void B_CollectAllStakes_Click(object sender, EventArgs e)
     {
         SAV.CollectAllStakes();
+        B_CollectAllStakes.Enabled = false;
         System.Media.SystemSounds.Asterisk.Play();
     }
 
     private void B_UnlockTMRecipes_Click(object sender, EventArgs e)
     {
         SAV.UnlockAllTMRecipes();
+        B_UnlockTMRecipes.Enabled = false;
         System.Media.SystemSounds.Asterisk.Play();
     }
 
     private void B_ActivateSnacksworthLegendaries_Click(object sender, EventArgs e)
     {
         SAV.ActivateSnacksworthLegendaries();
+        B_ActivateSnacksworthLegendaries.Enabled = false;
         System.Media.SystemSounds.Asterisk.Play();
     }
 
     private void B_UnlockCoaches_Click(object sender, EventArgs e)
     {
         SAV.UnlockAllCoaches();
+        B_UnlockCoaches.Enabled = false;
         System.Media.SystemSounds.Asterisk.Play();
     }
 
@@ -383,6 +382,7 @@ public partial class SAV_Trainer9 : Form
             accessor.GetBlock(block).ChangeBooleanType(SCTypeCode.Bool2);
         if (accessor.TryGetBlock("FSYS_RIDE_FLIGHT_ENABLE", out var fly))
             fly.ChangeBooleanType(SCTypeCode.Bool2); // Base & DLC1 saves do not have this block
+        B_UnlockBikeUpgrades.Enabled = false;
         System.Media.SystemSounds.Asterisk.Play();
     }
 
@@ -415,12 +415,14 @@ public partial class SAV_Trainer9 : Form
     {
         var accessor = SAV.Accessor;
         PlayerFashionUnlock9.UnlockBase(accessor, SAV.Gender);
+        B_UnlockClothing.Enabled = false;
         System.Media.SystemSounds.Asterisk.Play();
     }
 
     private void B_UnlockThrowStyles_Click(object sender, EventArgs e)
     {
         SAV.UnlockAllThrowStyles();
+        B_UnlockThrowStyles.Enabled = false;
         System.Media.SystemSounds.Asterisk.Play();
     }
 }
