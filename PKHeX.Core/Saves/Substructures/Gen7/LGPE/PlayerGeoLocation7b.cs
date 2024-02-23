65d8ab025dee518b2203d1adfca70bedda5e5f3d
Jonathan Herbert <foohyfooh@gmail.com>
2024-02-23 16:32:20 -0400

    Update PlayerGeoLocation7b To Match Changes To SaveBlock Constructor (#4194)

--- PKHeX.Core/Saves/Substructures/Gen7/LGPE/PlayerGeoLocation7b.cs
+++ PKHeX.Core/Saves/Substructures/Gen7/LGPE/PlayerGeoLocation7b.cs
@@ -2,9 +2,7 @@ using System;
 
 namespace PKHeX.Core;
 
-public sealed class PlayerGeoLocation7b : SaveBlock<SAV7b>
+public sealed class PlayerGeoLocation7b(SAV7b sav, int offset) : SaveBlock<SAV7b>(sav, offset)
 {
-    public PlayerGeoLocation7b(SAV7b sav, int offset) : base(sav) => Offset = offset;
-
     public Epoch1970Value AdventureBegin => new(Data.AsMemory(Offset + 0x48));
 }
