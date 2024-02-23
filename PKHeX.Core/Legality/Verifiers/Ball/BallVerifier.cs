292665a2af7a99583485cc08403b24ffeca61365
sora10pls <17801814+sora10pls@users.noreply.github.com>
2024-02-22 19:03:54 -0500

    Add latest distribution outbreak data 🍄🍒

--- PKHeX.Core/Legality/Verifiers/Ball/BallVerifier.cs
+++ PKHeX.Core/Legality/Verifiers/Ball/BallVerifier.cs
@@ -183,10 +183,6 @@ public sealed class BallVerifier : Verifier
         if (species is >= (int)Species.Sprigatito and <= (int)Species.Quaquaval)
             return VerifyBallEquals(ball, BallUseLegality.WildPokeballs8g_WithoutRaid);
 
-        // PLA Voltorb: Only via PLA (transfer only, not wild) and GO
-        if (enc is { Species: (ushort)Species.Voltorb, Form: 1 })
-            return VerifyBallEquals(ball, BallUseLegality.WildPokeballs8g_WithRaid);
-
         // S/V Tauros forms > 1: Only local Wild Balls for Blaze/Aqua breeds -- can't inherit balls from Kantonian/Combat.
         if (enc is { Species: (ushort)Species.Tauros, Form: > 1 })
             return VerifyBallEquals(ball, BallUseLegality.WildPokeballs9);
