92c964d6faaf0d9bedd86d99904216fd6910190b
Jonathan Herbert <foohyfooh@gmail.com>
2024-02-23 15:19:17 -0400

    Refactor And Deduplicate Some Switch Games Common Logic (#4187)
    - Improve Epoch 1900 classes using similar logic from PlayTime7b.
    - Move Time Classes into non Gen specific folder since it appears the logic is shared across a few of them.
    - Use Epoch1900DateTimeValue for LastSaved in PlayTime7b since the logic is the same.
    - Remove TeamIndexes9 since it is a duplicate of TeamIndexes9. Use the similar pattern like Box8 where it is reused in multiple locations.
    - Add BlueberryClubRoom9 to ISaveBlock9Main since it wasn't added when the class was introduced.
    - Simplify RaidSevenStar9 creation since GetBlockSafe does basically what the if-else block does.
    - Change SAV8SWSH Base Raid to RaidGalar to match the same way the SAV9SV does for its Base Raid.

--- PKHeX.Core/Saves/Substructures/Gen7/LGPE/PlayTime7b.cs
+++ PKHeX.Core/Saves/Substructures/Gen7/LGPE/PlayTime7b.cs
@@ -25,39 +25,34 @@ public sealed class PlayTime7b : SaveBlock<SAV7b>
         set => Data[Offset + 3] = (byte)value;
     }
 
-    private uint LastSaved { get => ReadUInt32LittleEndian(Data.AsSpan(Offset + 0x4)); set => WriteUInt32LittleEndian(Data.AsSpan(Offset + 0x4), value); }
-    private int LastSavedYear { get => (int)(LastSaved & 0xFFF) + 1900; set => LastSaved = (LastSaved & 0xFFFFF000) | (uint)(value - 1900); }
-    private int LastSavedMonth { get => (int)((LastSaved >> 12) & 0xF) + 1; set => LastSaved = (LastSaved & 0xFFFF0FFF) | (((uint)(value - 1) & 0xF) << 12); }
-    private int LastSavedDay { get => (int)((LastSaved >> 16) & 0x1F); set => LastSaved = (LastSaved & 0xFFE0FFFF) | (((uint)value & 0x1F) << 16); }
-    private int LastSavedHour { get => (int)((LastSaved >> 21) & 0x1F); set => LastSaved = (LastSaved & 0xFC1FFFFF) | (((uint)value & 0x1F) << 21); }
-    private int LastSavedMinute { get => (int)((LastSaved >> 26) & 0x3F); set => LastSaved = (LastSaved & 0x03FFFFFF) | (((uint)value & 0x3F) << 26); }
-    public string LastSavedTime => $"{LastSavedYear:0000}-{LastSavedMonth:00}-{LastSavedDay:00} {LastSavedHour:00}ː{LastSavedMinute:00}"; // not :
+    private Epoch1900DateTimeValue LastSaved => new(Data.AsMemory(Offset + 0x4));
+    public string LastSavedTime => $"{LastSaved.Year:0000}-{LastSaved.Month:00}-{LastSaved.Day:00} {LastSaved.Hour:00}ː{LastSaved.Minute:00}"; // not :
 
     public DateTime? LastSavedDate
     {
-        get => !DateUtil.IsDateValid(LastSavedYear, LastSavedMonth, LastSavedDay)
+        get => !DateUtil.IsDateValid(LastSaved.Year, LastSaved.Month, LastSaved.Day)
             ? null
-            : new DateTime(LastSavedYear, LastSavedMonth, LastSavedDay, LastSavedHour, LastSavedMinute, 0);
+            : new DateTime(LastSaved.Year, LastSaved.Month, LastSaved.Day, LastSaved.Hour, LastSaved.Minute, 0);
         set
         {
             // Only update the properties if a value is provided.
             if (value.HasValue)
             {
                 var dt = value.Value;
-                LastSavedYear = dt.Year;
-                LastSavedMonth = dt.Month;
-                LastSavedDay = dt.Day;
-                LastSavedHour = dt.Hour;
-                LastSavedMinute = dt.Minute;
+                LastSaved.Year = dt.Year;
+                LastSaved.Month = dt.Month;
+                LastSaved.Day = dt.Day;
+                LastSaved.Hour = dt.Hour;
+                LastSaved.Minute = dt.Minute;
             }
             else // Clear the date.
             {
                 // If code tries to access MetDate again, null will be returned.
-                LastSavedYear = 0;
-                LastSavedMonth = 0;
-                LastSavedDay = 0;
-                LastSavedHour = 0;
-                LastSavedMinute = 0;
+                LastSaved.Year = 0;
+                LastSaved.Month = 0;
+                LastSaved.Day = 0;
+                LastSaved.Hour = 0;
+                LastSaved.Minute = 0;
             }
         }
     }
