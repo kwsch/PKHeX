8fd53758bba61f021e59dd3abae38dbe260a63e8
Jonathan Herbert <foohyfooh@gmail.com>
2024-03-07 22:28:47 -0400

    Correct SM and USUM GameTime7 Offsets

--- PKHeX.Core/Saves/Substructures/Gen7/GameTime7.cs
+++ PKHeX.Core/Saves/Substructures/Gen7/GameTime7.cs
@@ -5,12 +5,12 @@ namespace PKHeX.Core;
 
 public sealed class GameTime7(SAV7 sav, Memory<byte> raw) : SaveBlock<SAV7>(sav, raw)
 {
-    public int ResumeYear { get => ReadInt32LittleEndian(Data[0x4..]); set => WriteInt32LittleEndian(Data[0x4..], value); }
-    public int ResumeMonth { get => Data[0x8]; set => Data[0x8] = (byte)value; }
-    public int ResumeDay { get => Data[0x9]; set => Data[0x9] = (byte)value; }
-    public int ResumeHour { get => Data[0xB]; set => Data[0xB] = (byte)value; }
-    public int ResumeMinute { get => Data[0xC]; set => Data[0xC] = (byte)value; }
-    public int ResumeSeconds { get => Data[0xD]; set => Data[0xD] = (byte)value; }
+    public int ResumeYear { get => ReadInt32LittleEndian(Data[0x04..]); set => WriteInt32LittleEndian(Data[0x04..], value); }
+    public int ResumeMonth { get => Data[0x08]; set => Data[0x08] = (byte)value; }
+    public int ResumeDay { get => Data[0x0C]; set => Data[0x0C] = (byte)value; }
+    public int ResumeHour { get => Data[0x14]; set => Data[0x14] = (byte)value; }
+    public int ResumeMinute { get => Data[0x18]; set => Data[0x18] = (byte)value; }
+    public int ResumeSeconds { get => Data[0x1C]; set => Data[0x1C] = (byte)value; }
     public uint SecondsToStart { get => ReadUInt32LittleEndian(Data[0x28..]); set => WriteUInt32LittleEndian(Data[0x28..], value); }
     public uint SecondsToFame { get => ReadUInt32LittleEndian(Data[0x30..]); set => WriteUInt32LittleEndian(Data[0x30..], value); }
 
