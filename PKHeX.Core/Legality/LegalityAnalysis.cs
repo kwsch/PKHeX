66e8bf264501006e27457b50fe6bc566e1889eba
Kurt <kaphotics@gmail.com>
2024-07-02 23:51:10 -0500

    Update 24.07.03
    Update wc9 pkl
    update dependencies (qrcoder got some perf improvements)
    deduplicate HOME tracker message in SV by consolidating the methods

--- PKHeX.Core/Legality/LegalityAnalysis.cs
+++ PKHeX.Core/Legality/LegalityAnalysis.cs
@@ -252,7 +252,7 @@ public sealed class LegalityAnalysis
     private void ParsePK9()
     {
         UpdateChecks();
-        Transfer.VerifyTransferLegalityG9(this);
+        Transfer.VerifyTransferLegalityG8(this);
     }
 
     /// <summary>
