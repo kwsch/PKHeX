9a5ad7e42a98947cb2bdf2764acbfbe5eaf7bfc9
Lusamine <30205550+Lusamine@users.noreply.github.com>
2024-10-21 00:08:33 -0500

    Handle crossovers in LGPE
    Ty @sora10pls, @LegoFigure11, @Skadiv, and @PP-theSLAYER for research!

--- PKHeX.Core/Legality/Encounters/Templates/Gen7b/EncounterArea7b.cs
+++ PKHeX.Core/Legality/Encounters/Templates/Gen7b/EncounterArea7b.cs
@@ -13,8 +13,17 @@ public sealed record EncounterArea7b : IEncounterArea<EncounterSlot7b>, IAreaLoc
     public GameVersion Version { get; }
 
     public readonly ushort Location;
+    public readonly byte ToArea1;
+    public readonly byte ToArea2; // None have more than 2 areas to feed into.
 
-    public bool IsMatchLocation(ushort location) => Location == location;
+    public bool IsMatchLocation(ushort location)
+    {
+        if (Location == location)
+            return true;
+
+        // Check if it matches either crossover location.
+        return location == ToArea1 || location == ToArea2;
+    }
 
     public static EncounterArea7b[] GetAreas(BinLinkerAccessor input, [ConstantExpected] GameVersion game)
     {
@@ -27,8 +36,10 @@ public sealed record EncounterArea7b : IEncounterArea<EncounterSlot7b>, IAreaLoc
     private EncounterArea7b(ReadOnlySpan<byte> data, [ConstantExpected] GameVersion game)
     {
         Location = ReadUInt16LittleEndian(data);
+        ToArea1 = data[2];
+        ToArea2 = data[3];
         Version = game;
-        Slots = ReadSlots(data[2..]);
+        Slots = ReadSlots(data[4..]);
     }
 
     private EncounterSlot7b[] ReadSlots(ReadOnlySpan<byte> data)
@@ -49,9 +60,10 @@ public sealed record EncounterArea7b : IEncounterArea<EncounterSlot7b>, IAreaLoc
     private EncounterSlot7b ReadSlot(ReadOnlySpan<byte> entry)
     {
         ushort species = entry[0]; // always < 255; only original 151
-        // form is always 0
+        // form is always 0 so is not included in this data.
+        byte flags = entry[1];
         byte min = entry[2];
         byte max = entry[3];
-        return new EncounterSlot7b(this, species, min, max);
+        return new EncounterSlot7b(this, species, min, max, flags);
     }
 }
