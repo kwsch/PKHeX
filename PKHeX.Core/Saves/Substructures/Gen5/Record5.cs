62b58f78e7970e46a7f3b239057e29462564c031
Squid <30328976+WonderSquid@users.noreply.github.com>
2024-11-09 09:08:57 -0600

    Labels for Gen 5 Trainer Records (plus renamed flag) (#4362)

--- PKHeX.Core/Saves/Substructures/Gen5/Record5.cs
+++ PKHeX.Core/Saves/Substructures/Gen5/Record5.cs
@@ -5,7 +5,14 @@ namespace PKHeX.Core;
 
 public class Record5(SAV5 SAV, Memory<byte> raw) : SaveBlock<SAV5>(SAV, raw)
 {
-    private Span<byte> DataRegion => Data[4..^4]; // 4..0x1DC
+    public const int Max32 = 999_999_999;
+    public const ushort Max16 = 65535;
+
+    public const byte Record32 = 68; // int32
+    public const byte Record16 = 100; // int16
+    private const byte Count = Record32 + Record16;
+
+    private Span<byte> DataRegion => Data[4..^4]; // 0..0x1DC
 
     private uint CryptoSeed // 0x1DC
     {
@@ -29,77 +36,45 @@ public class Record5(SAV5 SAV, Memory<byte> raw) : SaveBlock<SAV5>(SAV, raw)
         set => WriteUInt32LittleEndian(Data, value);
     }
 
-    public const int Record32 = 68;
-    public const int Record16 = 100;
+    public static ushort GetMax16(int recordID) => Max16;
+    public static uint GetMax32(int recordID) => Max32;
+
+    public static int GetOffset(int recordID) => recordID switch
+    {
+        < Record32 => (recordID * sizeof(int)),
+        < Count => (Record32 * sizeof(int)) + ((recordID - Record32) * sizeof(ushort)),
+        _ => -1,
+    };
+
     private const int Partition2 = Record32 * sizeof(uint);
     private Span<byte> Record32Data => DataRegion[..Partition2];
     private Span<byte> Record16Data => DataRegion[Partition2..];
 
-    private const uint Max32 = 999_999_999;
-    private const ushort Max16 = 9999;
-
     public uint GetRecord32(int index)
     {
-        ArgumentOutOfRangeException.ThrowIfGreaterThanOrEqual<uint>((uint)index, Record32);
+        ArgumentOutOfRangeException.ThrowIfGreaterThanOrEqual((uint)index, Record32);
         EnsureDecrypted();
         return ReadUInt32LittleEndian(Record32Data[(index * 4)..]);
     }
 
     public void SetRecord32(int index, uint value)
     {
-        ArgumentOutOfRangeException.ThrowIfGreaterThanOrEqual<uint>((uint)index, Record32);
+        ArgumentOutOfRangeException.ThrowIfGreaterThanOrEqual((uint)index, Record32);
         EnsureDecrypted();
-        WriteUInt32LittleEndian(Record32Data[(index * 4)..], Math.Min(Max32, value));
+        WriteUInt32LittleEndian(Record32Data[(index * 4)..], Math.Min(GetMax32(index), value));
     }
 
     public ushort GetRecord16(int index)
     {
-        ArgumentOutOfRangeException.ThrowIfGreaterThanOrEqual<uint>((uint)index, Record16);
+        ArgumentOutOfRangeException.ThrowIfGreaterThanOrEqual((uint)index, Record16);
         EnsureDecrypted();
         return ReadUInt16LittleEndian(Record16Data[(index * 2)..]);
     }
 
     public void SetRecord16(int index, ushort value)
     {
-        ArgumentOutOfRangeException.ThrowIfGreaterThanOrEqual<uint>((uint)index, Record16);
+        ArgumentOutOfRangeException.ThrowIfGreaterThanOrEqual((uint)index, Record16);
         EnsureDecrypted();
-        WriteUInt16LittleEndian(Record16Data[(index * 2)..], Math.Min(Max16, value));
-    }
-
-    public enum Record5Index
-    {
-        TimesSaved = 0,
-        StepsTaken = 1,
-        UsedBicycle = 2,
-        TotalBattles = 3,
-        WildBattles = 4,
-        TrainerBattles = 5,
-        Captured = 6,
-        CapturedFishing = 7,
-        EggsHatched = 8,
-        PokemonEvolved = 9,
-        TimesHealedPokeCenter = 10,
-
-        // ???
-
-        LinkTrades = 21,
-        LinkBattles = 22,
-        LinkBattleWins = 23,
-        LinkBattleLosses = 24,
-
-        // 00 - 0x110: start of u16 records
-        // 46 - 0x16C: Feeling Checks
-        // 47 - 0x16E: Musical
-        // 56 - 0x180: Battle Tests Attempted
-        // 57 - 0x182: Battle Test High Score
-        // 60 - 0x188: Customers
-        // 64 - 0x190: Movie Shoots
-        FirstU16             = Record32 + 00,
-        FeelingsChecked      = Record32 + 46,
-        Musical              = Record32 + 47,
-        BattleTestsAttempted = Record32 + 56,
-        BattleTestHighScore  = Record32 + 57,
-        Customers            = Record32 + 60,
-        MovieShoots          = Record32 + 64,
+        WriteUInt16LittleEndian(Record16Data[(index * 2)..], Math.Min(GetMax16(index), value));
     }
 }
