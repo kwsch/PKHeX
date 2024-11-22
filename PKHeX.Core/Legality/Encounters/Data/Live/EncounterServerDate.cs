a60e76f16d65a10566124ade72816f758a5df34e
9Bitdo <157295620+9Bitdo@users.noreply.github.com>
2024-11-22 11:43:17 +0800

    Add Operation Get Mythical Keldeo, Zarude & Deoxys's date (#4392)

--- PKHeX.Core/Legality/Encounters/Data/Live/EncounterServerDate.cs
+++ PKHeX.Core/Legality/Encounters/Data/Live/EncounterServerDate.cs
@@ -210,7 +210,10 @@ public static class EncounterServerDate
         {0512, (new(2024, 08, 16), new(2024, 08, 20))}, // Tomoya's Sylveon
         {0062, (new(2024, 10, 31), new(2026, 02, 01))}, // PokéCenter Birthday Tandemaus
         {0513, (new(2024, 11, 15), new(2024, 11, 23))}, // Patrick's Pelipper
-        
+        {0054, (new(2024, 11, 21), new(2025, 06, 01))}, // Operation Get Mythical's JPN Keldeo
+        {0055, (new(2024, 11, 21), new(2025, 06, 01))}, // Operation Get Mythical's JPN Zarude
+        {0056, (new(2024, 11, 21), new(2025, 06, 01))}, // Operation Get Mythical's JPN Deoxys
+       
         {9021, HOME3_ML}, // Hidden Ability Sprigatito
         {9022, HOME3_ML}, // Hidden Ability Fuecoco
         {9023, HOME3_ML}, // Hidden Ability Quaxly
