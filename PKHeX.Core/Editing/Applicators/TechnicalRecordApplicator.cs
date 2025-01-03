88c72568416eba1f89d7b3fa90f24180b413d4f6
Kurt <kaphotics@gmail.com>
2025-01-02 19:51:03 -0600

    Extract common technical record applications
    Get a neat API for it to provide an "option" of what all you want set.
    Ends up deduplicating some logic in batch editor as well.

--- PKHeX.Core/Editing/Applicators/TechnicalRecordApplicator.cs
+++ PKHeX.Core/Editing/Applicators/TechnicalRecordApplicator.cs
@@ -3,7 +3,7 @@ using System;
 namespace PKHeX.Core;
 
 /// <summary>
-/// Logic for modifying the Technical Record flags of a <see cref="PK8"/>.
+/// Logic for modifying the Technical Record flags of a <see cref="ITechRecord"/>.
 /// </summary>
 public static class TechnicalRecordApplicator
 {
@@ -123,4 +123,59 @@ public static class TechnicalRecordApplicator
         PK8 => IsRecordPermitted<PersonalTable8SWSH, PersonalInfo8SWSH>(evos, PersonalTable.SWSH, index),
         _ => false,
     };
+
+    public static void SetRecordFlags<T>(this T pk, TechnicalRecordApplicatorOption option)
+        where T : PKM, ITechRecord
+        => SetRecordFlags(pk, pk, option);
+
+    public static void SetRecordFlags(this ITechRecord record, PKM pk, TechnicalRecordApplicatorOption option)
+    {
+        record.ClearRecordFlags();
+        if (option is TechnicalRecordApplicatorOption.None)
+            return;
+        if (option is TechnicalRecordApplicatorOption.ForceAll)
+        {
+            record.SetRecordFlagsAll(true, record.Permit.RecordCountUsed);
+            return;
+        }
+        var la = new LegalityAnalysis(pk);
+        SetRecordFlagsInternal(record, pk, option, la);
+    }
+
+    public static void SetRecordFlags(this ITechRecord record, PKM pk, TechnicalRecordApplicatorOption option, LegalityAnalysis la)
+    {
+        record.ClearRecordFlags();
+        if (option is TechnicalRecordApplicatorOption.None)
+            return;
+        if (option is TechnicalRecordApplicatorOption.ForceAll)
+        {
+            record.SetRecordFlagsAll(true, record.Permit.RecordCountUsed);
+            return;
+        }
+        SetRecordFlagsInternal(record, pk, option, la);
+    }
+
+    private static void SetRecordFlagsInternal(ITechRecord record, PKM pk, TechnicalRecordApplicatorOption option, LegalityAnalysis la)
+    {
+        if (option is TechnicalRecordApplicatorOption.LegalCurrent)
+        {
+            Span<ushort> moves = stackalloc ushort[4];
+            pk.GetMoves(moves);
+            var evos = la.Info.EvoChainsAllGens.Get(pk.Context);
+            record.SetRecordFlags(moves, evos);
+        }
+        else if (option is TechnicalRecordApplicatorOption.LegalAll)
+        {
+            var evos = la.Info.EvoChainsAllGens.Get(pk.Context);
+            record.SetRecordFlagsAll(evos);
+        }
+    }
+}
+
+public enum TechnicalRecordApplicatorOption
+{
+    None,
+    ForceAll,
+    LegalCurrent,
+    LegalAll,
 }
