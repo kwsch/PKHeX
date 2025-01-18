c1d2babc5c2c3e51e0f7f3c83f964a08425cffbf
Kurt <kaphotics@gmail.com>
2025-01-17 22:08:38 -0600

    Set all purchased on MoveShop all

--- PKHeX.Core/Editing/Bulk/Suggestion/BatchModifications.cs
+++ PKHeX.Core/Editing/Bulk/Suggestion/BatchModifications.cs
@@ -58,9 +58,14 @@ internal static class BatchModifications
         if (e is IMasteryInitialMoveShop8 enc)
             enc.SetInitialMastery(pk);
         if (IsAll(propValue))
+        {
+            t.SetPurchasedFlagsAll();
             t.SetMoveShopFlagsAll(pk);
+        }
         else
+        {
             t.SetMoveShopFlags(pk);
+        }
         return ModifyResult.Modified;
     }
 
