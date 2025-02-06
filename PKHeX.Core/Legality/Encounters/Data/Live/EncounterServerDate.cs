1e16275d40712022470e372b0061ada36b7ec540
9Bitdo <157295620+9Bitdo@users.noreply.github.com>
2025-02-06 17:29:39 +0800

    Add Pokémon Day 2025 Flying Tera Type Eevee's date

--- PKHeX.Core/Legality/Encounters/Data/Live/EncounterServerDate.cs
+++ PKHeX.Core/Legality/Encounters/Data/Live/EncounterServerDate.cs
@@ -219,6 +219,7 @@ public static class EncounterServerDate
         {1012, (new(2024, 11, 21), new(2025, 06, 01))}, // Operation Get Mythical's KOR Zarude
         {1013, (new(2024, 11, 21), new(2025, 06, 01))}, // Operation Get Mythical's KOR Deoxys
         {1010, (new(2025, 01, 21), new(2025, 04, 01))}, // Pokémon Lucario & The Mystery of Mew Movie Gift KOR 아론's Lucario
+        {0514, (new(2025, 02, 05), new(2025, 07, 01))}, // Pokémon Day 2025 Flying Tera Type Eevee
 
         {9021, HOME3_ML}, // Hidden Ability Sprigatito
         {9022, HOME3_ML}, // Hidden Ability Fuecoco
