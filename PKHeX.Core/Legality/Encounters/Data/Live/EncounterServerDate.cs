e510733606a4d69033c3dbb49e24b9208081ca65
9Bitdo <157295620+9Bitdo@users.noreply.github.com>
2025-02-21 16:56:05 +0800

    Add Marco's Jumpluff date

--- PKHeX.Core/Legality/Encounters/Data/Live/EncounterServerDate.cs
+++ PKHeX.Core/Legality/Encounters/Data/Live/EncounterServerDate.cs
@@ -214,6 +214,7 @@ public static class EncounterServerDate
         {1013, new(2024, 11, 21, 2025, 06, 01)}, // Operation Get Mythical's KOR Deoxys
         {1010, new(2025, 01, 21, 2025, 04, 01)}, // Pokémon Lucario & The Mystery of Mew Movie Gift KOR 아론's Lucario
         {0514, new(2025, 02, 05, 2025, 07, 01, +2)}, // Pokémon Day 2025 Flying Tera Type Eevee
+        {0519, new(2025, 02, 20, 2025, 03, 01)}, // Marco's Jumpluff
 
         {9021, HOME3_ML}, // Hidden Ability Sprigatito
         {9022, HOME3_ML}, // Hidden Ability Fuecoco
