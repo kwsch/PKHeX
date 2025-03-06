0de0bd4c0b38685de5ecacb0acba3134e6fb5adc
Kurt <kwsch@users.noreply.github.com>
2025-03-06 09:37:26 -0600

    Add setting to allow colliding gen3/4 egg pidivs
    For those who think it is funny to RNG abuse egg PID/IVs to match a recognizable algorithm type. Opt-in setting because 99.9% of the time it's going to be a hack.
    #4347
    #3894
    #3092

--- PKHeX.Core/Legality/Encounters/Generator/ByGeneration/EncounterGenerator3.cs
+++ PKHeX.Core/Legality/Encounters/Generator/ByGeneration/EncounterGenerator3.cs
@@ -102,7 +102,7 @@ public sealed class EncounterGenerator3 : IEncounterGenerator
         // Errors will be flagged later for those not manually handled below.
         if (defer.Encounter is not { } lastResort)
             yield break;
-        if (defer.Type is DeferralType.PIDIV)
+        if (defer.Type is DeferralType.PIDIV && !(lastResort is EncounterEgg && ParseSettings.Settings.FramePattern.EggRandomAnyType3))
             info.ManualFlag = EncounterYieldFlag.InvalidPIDIV;
         else if (defer.Type is DeferralType.SlotNumber)
             info.ManualFlag = EncounterYieldFlag.InvalidFrame;
