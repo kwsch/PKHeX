c18611e10cb83e7336a15ce17651ffda65547143
taxicat1 <32942550+taxicat1@users.noreply.github.com>
2025-04-10 14:43:43 -0400

    Creator -> Leader in line with in-game terminology

--- PKHeX.Core/Saves/Substructures/Gen4/Group4.cs
+++ PKHeX.Core/Saves/Substructures/Gen4/Group4.cs
@@ -11,9 +11,9 @@ public sealed class Group4(Memory<byte> Raw)
 
     // Structure:
     // wchar[8] group name
-    // wchar[8] creator name
-    // u8 creator gender
-    // u8 creator lang
+    // wchar[8] leader name
+    // u8 leader gender
+    // u8 leader lang
     // u16 alignment
     // u32 origin seed (used as identifier)
     // u32 current seed
@@ -25,18 +25,18 @@ public sealed class Group4(Memory<byte> Raw)
     public string Name
     {
         get => StringConverter4.GetString(NameTrash);
-        set => StringConverter4.SetString(NameTrash, value, 7, CreatorLang, StringConverterOption.None);
+        set => StringConverter4.SetString(NameTrash, value, 7, LeaderLang, StringConverterOption.None);
     }
 
-    private Span<byte> CreatorTrash => Data.Slice(0x10, 16);
-    public string Creator
+    private Span<byte> LeaderTrash => Data.Slice(0x10, 16);
+    public string Leader
     {
-        get => StringConverter4.GetString(CreatorTrash);
-        set => StringConverter4.SetString(CreatorTrash, value, 7, CreatorLang, StringConverterOption.None);
+        get => StringConverter4.GetString(LeaderTrash);
+        set => StringConverter4.SetString(LeaderTrash, value, 7, LeaderLang, StringConverterOption.None);
     }
     
-    public byte CreatorGender { get => Data[0x20]; set => Data[0x20] = value; }
-    public byte CreatorLang   { get => Data[0x21]; set => Data[0x21] = value; }
+    public byte LeaderGender { get => Data[0x20]; set => Data[0x20] = value; }
+    public byte LeaderLang   { get => Data[0x21]; set => Data[0x21] = value; }
 
     public uint Id   { get => ReadUInt32LittleEndian(Data[0x24..]); set => WriteUInt32LittleEndian(Data[0x24..], value); }
     public uint Seed { get => ReadUInt32LittleEndian(Data[0x28..]); set => WriteUInt32LittleEndian(Data[0x28..], value); }
