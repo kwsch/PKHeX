e72995e8981265d07ffe3b446df1e119d0a2bfa4
Manu <52102823+Manu098vm@users.noreply.github.com>
2025-05-21 05:53:32 +0200

    Standardize IGenerateSeed32 API for gen9 raid encounters (#4497)

--- PKHeX.Core/Legality/Encounters/Templates/Gen9/EncounterMight9.cs
+++ PKHeX.Core/Legality/Encounters/Templates/Gen9/EncounterMight9.cs
@@ -3,8 +3,7 @@ using static System.Buffers.Binary.BinaryPrimitives;
 
 namespace PKHeX.Core;
 
-public sealed record EncounterMight9
-    : IEncounterable, IEncounterMatch, IEncounterConvertible<PK9>, ITeraRaid9, IMoveset, IFlawlessIVCount, IFixedGender, IFixedNature
+public sealed record EncounterMight9 : ITeraRaid9, IFixedNature
 {
     public byte Generation => 9;
     ushort ILocation.Location => Location;
@@ -249,8 +248,6 @@ public sealed record EncounterMight9
     };
 
     #region Generating
-    PKM IEncounterConvertible.ConvertToPKM(ITrainerInfo tr, EncounterCriteria criteria) => ConvertToPKM(tr, criteria);
-    PKM IEncounterConvertible.ConvertToPKM(ITrainerInfo tr) => ConvertToPKM(tr);
     public PK9 ConvertToPKM(ITrainerInfo tr) => ConvertToPKM(tr, EncounterCriteria.Unrestricted);
     public PK9 ConvertToPKM(ITrainerInfo tr, EncounterCriteria criteria)
     {
@@ -285,19 +282,33 @@ public sealed record EncounterMight9
         return pk;
     }
 
+    private PersonalInfo9SV GetPersonal() => PersonalTable.SV[Species, Form];
+
     private void SetPINGA(PK9 pk, EncounterCriteria criteria, PersonalInfo9SV pi)
     {
-        const byte rollCount = 1;
-        const byte undefinedSize = 0;
-        byte gender = GetGender(pi);
-        var param = new GenerateParam9(Species, gender, FlawlessIVCount, rollCount,
-            undefinedSize, undefinedSize, ScaleType, Scale,
-            Ability, Shiny, Nature, IVs);
-
+        var param = GetParam(pi);
         var init = Util.Rand.Rand64();
         var success = this.TryApply32(pk, init, param, criteria);
         if (!success && !this.TryApply32(pk, init, param, criteria.WithoutIVs()))
             this.TryApply32(pk, init, param, EncounterCriteria.Unrestricted);
+    }
+
+    private GenerateParam9 GetParam(PersonalInfo9SV pi)
+    {
+        const byte rollCount = 1;
+        const byte undefinedSize = 0;
+        byte gender = GetGender(pi);
+        return new GenerateParam9(Species, gender, FlawlessIVCount, rollCount,
+            undefinedSize, undefinedSize, ScaleType, Scale,
+            Ability, Shiny, Nature, IVs);
+    }
+
+    public bool GenerateSeed32(PKM pk, uint seed)
+    {
+        var pk9 = (PK9)pk;
+        var param = GetParam(GetPersonal());
+        Encounter9RNG.GenerateData(pk9, param, EncounterCriteria.Unrestricted, seed);
+        return true;
     }
     #endregion
 
