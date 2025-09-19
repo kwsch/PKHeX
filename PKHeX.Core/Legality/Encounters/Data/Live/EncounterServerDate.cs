e8d3acb9389218f0d1e7a4705525c8cb5d5e2aa3
sora10pls <17801814+sora10pls@users.noreply.github.com>
2025-09-18 20:04:08 -0400

    Update Shiny Chi-Yu Wonder Card ID

--- PKHeX.Core/Legality/Encounters/Data/Live/EncounterServerDate.cs
+++ PKHeX.Core/Legality/Encounters/Data/Live/EncounterServerDate.cs
@@ -225,7 +225,7 @@ public static class EncounterServerDate
         {1542, new(2025, 08, 07, 2025, 10, 01)}, // Shiny Wo-Chien
         {1544, new(2025, 08, 21, 2025, 10, 01)}, // Shiny Chien-Pao
         {1546, new(2025, 09, 04, 2025, 10, 01)}, // Shiny Ting-Lu
-        {9999, new(2025, 09, 18, 2025, 10, 01)}, // Shiny Chi-Yu
+        {1548, new(2025, 09, 18, 2025, 10, 01)}, // Shiny Chi-Yu
         {0524, new(2025, 08, 14, 2025, 08, 31)}, // WCS 2025 Toedscool
         {0525, new(2025, 08, 15, 2025, 08, 23)}, // WCS 2025 Luca Ceribelli's Farigiraf
 
