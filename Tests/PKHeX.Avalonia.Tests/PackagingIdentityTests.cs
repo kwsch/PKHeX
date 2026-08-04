9840bea5e3a487f83b13ef3a6de54bc8282cb294
doctorllll <qwejoyfd@gmail.com>
2026-08-04 22:44:35 +0800

    fix: update packaging identity test for fork

--- Tests/PKHeX.Avalonia.Tests/PackagingIdentityTests.cs
+++ Tests/PKHeX.Avalonia.Tests/PackagingIdentityTests.cs
@@ -6,30 +6,30 @@ public class PackagingIdentityTests
     public void WindowsPackaging_UsesPublicApplicationIdentity()
     {
         var installer = ReadRepositoryFile("packaging", "windows", "installer.iss");
-        var wingetLocale = ReadRepositoryFile("packaging", "winget", "realgarit.PKHeXAvalonia.locale.en-US.yaml");
-        var wingetVersion = ReadRepositoryFile("packaging", "winget", "realgarit.PKHeXAvalonia.yaml");
-        var wingetInstaller = ReadRepositoryFile("packaging", "winget", "realgarit.PKHeXAvalonia.installer.yaml");
+        var wingetLocale = ReadRepositoryFile("packaging", "winget", "doctorllll.PKHeXAvalonia.locale.en-US.yaml");
+        var wingetVersion = ReadRepositoryFile("packaging", "winget", "doctorllll.PKHeXAvalonia.yaml");
+        var wingetInstaller = ReadRepositoryFile("packaging", "winget", "doctorllll.PKHeXAvalonia.installer.yaml");
         var buildProperties = ReadRepositoryFile("Directory.Build.props");
         var applicationProject = ReadRepositoryFile("PKHeX.Avalonia", "PKHeX.Avalonia.csproj");
         var signingCertificateScript = ReadRepositoryFile("Scripts", "make-signing-cert.sh");
 
         AssertHasLine(installer, "#define MyAppName \"PKHeX-Avalonia\"");
         AssertHasLine(installer, "#define MyAppPublisher \"Patrik Lleshaj\"");
-        AssertHasLine(installer, "#define MyAppURL \"https://github.com/realgarit/PKHeX-Avalonia\"");
+        AssertHasLine(installer, "#define MyAppURL \"https://github.com/doctorllll/PKHeX\"");
         AssertHasLine(installer, "AppId={{B6C9F1B4-7B7B-4B7A-9C2E-8B6C9C7B7B7B}}");
         AssertHasLine(installer, "AppVerName={#MyAppName}");
         AssertHasLine(installer, "UninstallDisplayName={#MyAppName}");
 
         foreach (var manifest in new[] { wingetLocale, wingetVersion, wingetInstaller })
-            AssertHasLine(manifest, "PackageIdentifier: realgarit.PKHeXAvalonia");
+            AssertHasLine(manifest, "PackageIdentifier: doctorllll.PKHeXAvalonia");
 
         AssertHasLine(wingetLocale, "Publisher: Patrik Lleshaj");
-        AssertHasLine(wingetLocale, "PublisherUrl: https://github.com/realgarit");
-        AssertHasLine(wingetLocale, "PublisherSupportUrl: https://github.com/realgarit/PKHeX-Avalonia/issues");
+        AssertHasLine(wingetLocale, "PublisherUrl: https://github.com/doctorllll");
+        AssertHasLine(wingetLocale, "PublisherSupportUrl: https://github.com/doctorllll/PKHeX/issues");
         AssertHasLine(wingetLocale, "PackageName: PKHeX-Avalonia");
-        AssertHasLine(wingetLocale, "PackageUrl: https://github.com/realgarit/PKHeX-Avalonia");
-        AssertHasLine(wingetLocale, "LicenseUrl: https://github.com/realgarit/PKHeX-Avalonia/blob/main/LICENSE");
-        AssertHasLine(wingetInstaller, "InstallerUrl: https://github.com/realgarit/PKHeX-Avalonia/releases/download/v{{VERSION}}/PKHeX-Avalonia-Setup.exe");
+        AssertHasLine(wingetLocale, "PackageUrl: https://github.com/doctorllll/PKHeX");
+        AssertHasLine(wingetLocale, "LicenseUrl: https://github.com/doctorllll/PKHeX/blob/master/LICENSE");
+        AssertHasLine(wingetInstaller, "InstallerUrl: https://github.com/doctorllll/PKHeX/releases/download/v{{VERSION}}/PKHeX-Avalonia-Setup.exe");
 
         AssertHasLine(applicationProject, "<Product>PKHeX-Avalonia</Product>");
         AssertHasLine(applicationProject, "<AssemblyTitle>PKHeX-Avalonia</AssemblyTitle>");
